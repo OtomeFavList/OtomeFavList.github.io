401f58b5b986efade5f824f2da4eb5a8e98c7b31
OtomeFavList <otomefavlist@163.com>
2026-08-01 01:42:06 +0800

    Add initial games data to games.js

---
+++ data/games.js
@@ -0,0 +1,18 @@
+const games = [
+
+    {
+        id: "cm",
+        name: "Collar×Malice"
+    },
+
+    {
+        id: "cr",
+        name: "Code:Realize"
+    },
+
+    {
+        id: "hakuoki",
+        name: "薄樱鬼"
+    }
+
+];
