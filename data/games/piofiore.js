a61cade13656c48b1426c2f0025bfb327a6a81bd
OtomeFavList <otomefavlist@163.com>
2026-08-01 12:28:18 +0800

    Create piofiore.js

---
+++ data/games/piofiore.js
@@ -0,0 +1,108 @@
+// ======================================
+// Piofiore no Banshou
+// data/games/piofiore.js
+// ======================================
+
+const piofiore = [
+
+{
+
+    id: "piofiore",
+
+    name: "虔诚之花的晚钟",
+
+    englishName: "Piofiore: Fated Memories",
+
+    shortName: "Piofiore",
+
+    cover: "images/piofiore/cover.webp",
+
+    heroine: {
+
+        id: "liliana",
+
+        name: "莉莉安娜",
+
+        image: "images/piofiore/heroine.webp"
+
+    },
+
+    characters: [
+
+        {
+
+            id: "dante",
+
+            name: "但丁",
+
+            image: "images/piofiore/dante.webp",
+
+            hidden: false
+
+        },
+
+        {
+
+            id: "nicola",
+
+            name: "尼古拉",
+
+            image: "images/piofiore/nicola.webp",
+
+            hidden: false
+
+        },
+
+        {
+
+            id: "yang",
+
+            name: "杨",
+
+            image: "images/piofiore/yang.webp",
+
+            hidden: false
+
+        },
+
+        {
+
+            id: "gilbert",
+
+            name: "吉尔伯特",
+
+            image: "images/piofiore/gilbert.webp",
+
+            hidden: false
+
+        },
+
+        {
+
+            id: "orlok",
+
+            name: "奥洛克",
+
+            image: "images/piofiore/orlok.webp",
+
+            hidden: false
+
+        },
+
+        {
+
+            id: "henri",
+
+            name: "亨利",
+
+            image: "images/piofiore/henri.webp",
+
+            hidden: true
+
+        }
+
+    ]
+
+}
+
+];
