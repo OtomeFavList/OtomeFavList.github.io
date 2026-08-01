92b982af555785ba59937c50ea166c0784d3b6a1
OtomeFavList <otomefavlist@163.com>
2026-08-01 12:26:54 +0800

    Create template.js

---
+++ data/games/template.js
@@ -0,0 +1,51 @@
+const templateGame = [
+
+{
+
+    id:"game-id",
+
+    name:"Game Name",
+
+    cover:"images/game-id/cover.webp",
+
+    heroine:{
+
+        id:"heroine",
+
+        name:"Heroine",
+
+        image:"images/game-id/heroine.webp"
+
+    },
+
+    characters:[
+
+        {
+
+            id:"character01",
+
+            name:"Character",
+
+            image:"images/game-id/character01.webp",
+
+            hidden:false
+
+        },
+
+        {
+
+            id:"character02",
+
+            name:"Character",
+
+            image:"images/game-id/character02.webp",
+
+            hidden:false
+
+        }
+
+    ]
+
+}
+
+];
