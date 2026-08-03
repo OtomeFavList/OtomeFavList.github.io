592c4eb515ac1b6f4584484a53f07c6c64180cf0
OtomeFavList <otomefavlist@163.com>
2026-08-04 00:33:46 +0800

    Update game001.js

--- data/games/game001.js
+++ data/games/game001.js
@@ -4,30 +4,77 @@
 // 无需修改data/games.js，仅需要在data/games.js的allGameFiles数组追加本文件路径
 const singleGameData = {
     id: "game001", // 全局唯一ID，不可重复
-    name: "示例乙女游戏1",
+    name: "虔诚之花的晚钟",
     year: "2020",
-    publisher: "原厂发行商",
-    cnStudio: "汉化组/汉化厂商",
-    writer: "剧本编剧",
-    art: "原画画师",
-    cover: "img/game/game001_cover.jpg", // 封面图路径，统一前缀img/
+    publisher: "Otomate",
+    cnStudio: "GSE",
+    writer: "かずら林檎",
+    art: "RiRi",
+    cover: "img/game/001.jpg", // 封面图路径，统一前缀img/
     charList: [
         // 女主模板 female
         {
             id: "g001_f01",
-            name: "女主A",
+            name: "莉莉安娜",
             gender: "female",
-            isHidden: false,    // 角色本体：true=隐藏角色，全局/游戏单独开关控制是否展示该角色
-            isFD: false,        // 角色本体：true=续作/FD专属角色，FD开关控制是否展示该角色
+            isHidden: false,
+            isFD: false,
             images: [
-                { src: "img/char/g001_f01_1.jpg", type: "base" },      // 默认基础图，始终加载
-                { src: "img/char/g001_f01_2.jpg", type: "hidden" }     // 需要开启隐藏角色开关才展示的图片
+                { src: "img/char/001/Liliana.jpg", type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 普通可攻略男主 male
         {
             id: "g001_m01",
-            name: "男主1",
+            name: "但丁·法尔宗",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { src: "img/char/001/Dante.jpg", type: "base" },
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g001_m02",
+            name: "吉尔伯特·烈福",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { src: "img/char/g001_m01_1.jpg", type: "base" },
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g001_m03",
+            name: "但丁·法尔宗",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { src: "img/char/g001_m01_1.jpg", type: "base" },
+                { src: "img/char/g001_m01_2.jpg", type: "hidden" },
+                { src: "img/char/g001_m01_3.jpg", type: "fd" }          // 需要开启FD开关才展示的图片
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g001_m04",
+            name: "但丁·法尔宗",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { src: "img/char/g001_m01_1.jpg", type: "base" },
+                { src: "img/char/g001_m01_2.jpg", type: "hidden" },
+                { src: "img/char/g001_m01_3.jpg", type: "fd" }          // 需要开启FD开关才展示的图片
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g001_m05",
+            name: "但丁·法尔宗",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -40,7 +87,7 @@ const singleGameData = {
         // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
         {
             id: "g001_h01",
-            name: "隐藏攻略角色",
+            name: "亨利·兰伯特",
             gender: "male",
             isHidden: true,
             isFD: false,
@@ -48,17 +95,6 @@ const singleGameData = {
                 { src: "img/char/g001_h01_1.jpg", type: "base" }
             ]
         },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g001_fd01",
-            name: "FD新增角色",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            images: [
-                { src: "img/char/g001_fd01_1.jpg", type: "base" }
-            ]
-        }
     ]
 };
 
