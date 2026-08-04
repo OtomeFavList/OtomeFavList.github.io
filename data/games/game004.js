a17a3f456ed9febccac180b85e8b6aac52c47a66
OtomeFavList <otomefavlist@163.com>
2026-08-05 01:20:53 +0800

    Update game004.js

--- data/games/game004.js
+++ data/games/game004.js
@@ -4,78 +4,129 @@
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 const gameData = {
     id: "game004",
-    name: "幻奏咖啡厅-Enchanté-",
-    year: "2020",
-    publisher: "Otomate",
-    cnStudio: "GSE",
-    writer: ["中山智美","佐々木麿","吉村りりか","小縞なお","高羽朋美"],
-    art: "ユウヤ",
-    cover: "img/game/003.jpg",
+    name: "蒸汽监狱",
+    year: "2021",
+    publisher: "HuneX ",
+    cnStudio: "",
+    writer: ["Yu-masu"],
+    art: "一色箱",
+    cover: "img/game/004.jpg",
     charList: [
-        // 淡木琴音
+        // 琪丝･提斯特拉
         {
             id: "g004_f01",
-            name: "淡木琴音",
+            name: "琪丝･提斯特拉",
             gender: "female",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Awaki.jpg","img/char/003/Awaki2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Cyrus.jpg","img/char/004/Cyrus2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Cyrus3.jpg","img/char/004/Cyrus4.jpg"], type: "hidden" }
             ]
         },
-        // 卡努斯·埃斯帕达
+        // 亚达钧
         {
             id: "g004_m01",
-            name: "卡努斯·埃斯帕达",
+            name: "亚达钧",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Canus.jpg","img/char/003/Canus2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Adage.jpg","img/char/004/Adage2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Adage3.jpg"], type: "hidden" }
             ]
         },
-        // 伊格尼斯·卡里本库鲁斯
+        // 埃尔托克里德･瓦伦丁
         {
             id: "g004_m02",
-            name: "伊格尼斯·卡里本库鲁斯",
+            name: "埃尔托克里德･瓦伦丁",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Ignis.jpg","img/char/003/Ignis2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Eltcreed.jpg","img/char/004/Eltcreed2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Eltcreed3.jpg"], type: "hidden" }
             ]
         },
-        // 伊尔·法多·德·里艾
+        // 芬･尤克列斯
         {
             id: "g004_m03",
-            name: "伊尔·法多·德·里艾",
+            name: "芬･尤克列斯",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Il.jpg","img/char/003/Il2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Fin.jpg","img/char/004/Fin2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Fin3.jpg","img/char/004/Fin4.jpg"], type: "hidden" }
             ]
         },
-        // 米歇尔·阿雷克斯
+        // 伊内斯･海因里希･海涅
         {
             id: "g004_m04",
-            name: "米歇尔·阿雷克斯",
+            name: "伊内斯･海因里希･海涅",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Misyr.jpg","img/char/003/Misyr2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Ines.jpg","img/char/004/Ines2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Ines3.jpg"], type: "hidden" }
             ]
         },
-        // 凜堂香
+        // 乌尔利克･费里尔
         {
             id: "g004_m05",
-            name: "凜堂香",
+            name: "乌尔利克･费里尔",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/003/Rindou.jpg","img/char/003/Rindou2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Ulrik.jpg","img/char/004/Ulrik2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Ulrik3.jpg"], type: "hidden" }
+            ]
+        }
+        // 尤尼･石英
+        {
+            id: "g004_m06",
+            name: "尤尼･石英",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/004/Yune.jpg","img/char/004/Yune2.jpg"], type: "base" }
+                { srcList: ["img/char/004/Yune3.jpg], type: "hidden" }
+            ]
+        }
+        // 杰里姆
+        {
+            id: "g004_fd01",
+            name: "杰里姆",
+            gender: "male",
+            isHidden: false,
+            isFD: true,
+            images: [
+                { srcList: ["img/char/004/Jereme.jpg"], type: "base" }
+            ]
+        }
+        // 杰比特
+        {
+            id: "g004_fd02",
+            name: "杰比特",
+            gender: "male",
+            isHidden: false,
+            isFD: true,
+            images: [
+                { srcList: ["img/char/004/Jevite.jpg"], type: "base" }
+            ]
+        }
+        // 萨克森‧布兰登堡
+        {
+            id: "g004_fd03",
+            name: "萨克森‧布兰登堡",
+            gender: "male",
+            isHidden: false,
+            isFD: true,
+            images: [
+                { srcList: ["img/char/004/Sachsen.jpg"], type: "base" }
             ]
         }
     ]
