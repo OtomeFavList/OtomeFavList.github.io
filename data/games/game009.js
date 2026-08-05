18f26852c92027c1f59d661bcebfe5c2d1b482b0
OtomeFavList <otomefavlist@163.com>
2026-08-06 02:31:27 +0800

    Update game data for game009

---
+++ data/games/game009.js
@@ -0,0 +1,107 @@
+// data/games/game009.js
+// ==========【单个游戏独立数据模板｜新版ESModule】==========
+// 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
+// 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
+const gameData = {
+    id: "game009",
+    name: "项圈×恶意",
+    year: "2021",
+    publisher: "Otomate",
+    cnStudio: "JSD",
+    writer: ["有野幸","砂原有季","いわた志信","小縞なお"],
+    art: "花邑舞",
+    cover: "img/game/009.jpg",
+    charList: [
+        // 星野市香
+        {
+            id: "g009_f01",
+            name: "星野市香",
+            gender: "female",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Ichika.jpg","img/char/007/Ichika2.jpg"], type: "base" }
+            ]
+        },
+        // 柳爱时
+        {
+            id: "g009_m01",
+            name: "柳爱时",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Aiji.jpg"], type: "base" }
+            ]
+        },
+        // 冈崎契
+        {
+            id: "g009_m02",
+            name: "冈崎契",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Kei.jpg"], type: "base" }
+            ]
+        },
+        // 榎本峰雄
+        {
+            id: "g009_m03",
+            name: "榎本峰雄",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Mineo.jpg"], type: "base" }
+            ]
+        },
+        // 白石景之
+        {
+            id: "g009_m04",
+            name: "白石景之",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Shiraishi.jpg"], type: "base" }
+            ]
+        },
+        // 笹塚尊
+        {
+            id: "g009_m05",
+            name: "笹塚尊",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/007/Takeru.jpg"], type: "base" }
+            ]
+        }
+      // 冴木弓弦
+        {
+            id: "g009_h01",
+            name: "隐藏攻略角色",
+            gender: "male",
+            isHidden: true,
+            isFD: true,
+            images: [
+                { src: "img/char/007/Yuzuru.jpg", type: "base" }
+            ]
+        },
+        // 峰岸誠司
+        {
+            id: "g009_fd01",
+            name: "峰岸誠司",
+            gender: "male",
+            isHidden: false,
+            isFD: true,
+            images: [
+                { src: "img/char/007/Shiraishi.jpg", type: "base" }
+            ]
+        }
+    ]
+};
+
+// ESModule导出
+export { gameData };
