86dcc24a77146eb9312fbe1f2dd05a9e6a9ed902
OtomeFavList <otomefavlist@163.com>
2026-08-07 23:27:55 +0800

    Update game020.js

--- data/games/game020.js
+++ data/games/game020.js
@@ -6,6 +6,7 @@ const gameData = {
     name: "不可逾越的红花 ~双月~",
     year: "2023",
     publisher: "HuneX",
+    cnStudio: "dramatic create",
     writer: ["松竹梅"],
     art: "月野御豆",
     cover: "img/game/020.jpg", // 封面图路径，统一前缀img/
