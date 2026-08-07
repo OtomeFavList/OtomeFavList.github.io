e3650a2e33a8b42217844e270cf65ce68b3e9338
OtomeFavList <otomefavlist@163.com>
2026-08-08 07:33:14 +0800

    Create game029.js

---
+++ data/games/game029.js
@@ -0,0 +1,106 @@
+// ==========【单个游戏独立数据模板｜新版ESModule】==========
+// 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
+// 无需额外配置，仅需要到 main.js 顶部 🚨gameIdList数组追加编号"002","003"...
+const gameData = {
+    id: "game029", // 全局唯一ID，不可重复
+    name: "示例乙女游戏",
+    year: "2023",
+    publisher: "Otomate",
+    cnStudio: "JSD",
+    writer: ["関涼子","魚住ユキコ","上月はじめ","吉村りりか","恵村まお","夏野景","神城咲弥","夜空茜","石倉みもり","花井カオリ","猫乃しおり","まるや諒","天乃聖樹","センチメンタルべにこ","七瀬みお","仰木サヤ","柿本悠理","祁答院慎","紅原香"],
+    art: "もちもちた",
+    cover: "img/game/029.jpg", // 封面图路径，统一前缀img/
+    charList: [
+        // 女主模板 female
+        {
+            id: "g029_f01",
+            name: "女主A",
+            gender: "female",
+            isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
+            isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
+            images: [
+                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 女主模板 female
+        {
+            id: "g029_f02",
+            name: "女主A",
+            gender: "female",
+            isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
+            isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
+            images: [
+                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m01",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m02",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m03",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m04",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m05",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g029_m06",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        }
+    ]
+};
+
+// ✅新版导出！不要使用window.gameDataList.push！
+export { gameData };
