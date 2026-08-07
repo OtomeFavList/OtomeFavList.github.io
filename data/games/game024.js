7866ab7c7845773673395d99d6080872f4fc0df6
OtomeFavList <otomefavlist@163.com>
2026-08-08 04:27:06 +0800

    Update game024.js

--- data/games/game024.js
+++ data/games/game024.js
@@ -2,19 +2,19 @@
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
 // 无需额外配置，仅需要到 main.js 顶部 🚨gameIdList数组追加编号"002","003"...
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复
-    name: "KLAP!! ~愛與懲罰~",
+    id: "game024", // 全局唯一ID，不可重复
+    name: "KLAP!! ~爱与惩罚~",
     year: "2023",
     publisher: "Otomate",
     cnStudio: "JOYOLAND",
-    writer: ["剧本编剧","剧本编剧","剧本编剧","剧本编剧"],
-    art: "原画画师",
-    cover: "img/game/0.jpg", // 封面图路径，统一前缀img/
+    writer: ["皆川千尋","水都","春夏秋冬よもひろ","夏越ちか","狐塚冬里","城戸蘭丸","駒豆羅々子","寺須ハウス","高林祐樹","有野幸","佐々木麿"],
+    art: "川人やすた",
+    cover: "img/game/024.jpg", // 封面图路径，统一前缀img/
     charList: [
-        // 女主模板 female
+        // 山城历
         {
-            id: "g0_f01",
-            name: "女主A",
+            id: "g024_f01",
+            name: "山城历",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
@@ -26,7 +26,7 @@ const gameData = {
         },
         // 普通可攻略男主 male
         {
-            id: "g0_m01",
+            id: "g024_m01",
             name: "男主1",
             gender: "male",
             isHidden: false,
@@ -39,7 +39,7 @@ const gameData = {
         },
         // 普通可攻略男主 male
         {
-            id: "g0_m02",
+            id: "g024_m02",
             name: "男主1",
             gender: "male",
             isHidden: false,
@@ -52,7 +52,7 @@ const gameData = {
         },
         // 普通可攻略男主 male
         {
-            id: "g0_m03",
+            id: "g024_m03",
             name: "男主1",
             gender: "male",
             isHidden: false,
@@ -65,7 +65,7 @@ const gameData = {
         },
         // 普通可攻略男主 male
         {
-            id: "g0_m04",
+            id: "g024_m04",
             name: "男主1",
             gender: "male",
             isHidden: false,
@@ -78,7 +78,7 @@ const gameData = {
         },
         // 普通可攻略男主 male
         {
-            id: "g0_m05",
+            id: "g024_m05",
             name: "男主1",
             gender: "male",
             isHidden: false,
@@ -89,26 +89,43 @@ const gameData = {
                 { srcList: ["img/char/g0_m01_4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // 普通可攻略男主 male
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["img/char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
+            id: "g024_m06",
+            name: "男主1",
             gender: "male",
             isHidden: false,
-            isFD: true,
+            isFD: false,
             images: [
-                { srcList: ["img/char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/g0_m01_3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
+                { srcList: ["img/char/g0_m01_4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g024_m07",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/g0_m01_3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
+                { srcList: ["img/char/g0_m01_4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g024_m08",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/g0_m01_3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
+                { srcList: ["img/char/g0_m01_4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         }
     ]
