e41f759c5d2fa3e39248b3ed218e17b726305abb
OtomeFavList <otomefavlist@163.com>
2026-08-08 20:26:44 +0800

    Create game040.js

---
+++ data/games/game040.js
@@ -0,0 +1,95 @@
+// ==========【单个游戏独立数据模板｜新版ESModule】==========
+// 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
+// 无需额外配置，仅需要到 main.js 顶部 🚨gameIdList数组追加编号"002","003"...
+const gameData = {
+    id: "game040", // 全局唯一ID，不可重复
+    name: "示例乙女游戏",
+    year: "20",
+    publisher: "原厂发行商",
+    cnStudio: "汉化组/汉化厂商",
+    writer: ["剧本编剧","剧本编剧","剧本编剧","剧本编剧"],
+    art: "原画画师",
+    cover: "img/game/0.jpg", // 封面图路径，统一前缀img/
+    charList: [
+        // 女主模板 female
+        {
+            id: "g0_f01",
+            name: "女主A",
+            gender: "female",
+            isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
+            isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
+            images: [
+                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g0_m01",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g0_m02",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g0_m03",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g0_m04",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 普通可攻略男主 male
+        {
+            id: "g0_m05",
+            name: "男主1",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+            ]
+        },
+        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        {
+            id: "g0_h01",
+            name: "隐藏攻略角色",
+            gender: "male",
+            isHidden: true,
+            isFD: false,
+            images: [
+                { srcList: ["img/char/g001_h01_1.jpg"], type: "base" }
+            ]
+        }
+    ]
+};
+
+// ✅新版导出！不要使用window.gameDataList.push！
+export { gameData };
