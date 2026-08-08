c5dd914ec91c0954d21de796be50fdc8517160cf
OtomeFavList <otomefavlist@163.com>
2026-08-08 20:43:08 +0800

    Update game040.js

--- data/games/game040.js
+++ data/games/game040.js
@@ -3,89 +3,89 @@
 // 无需额外配置，仅需要到 main.js 顶部 🚨gameIdList数组追加编号"002","003"...
 const gameData = {
     id: "game040", // 全局唯一ID，不可重复
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: "原厂发行商",
-    cnStudio: "汉化组/汉化厂商",
-    writer: ["剧本编剧","剧本编剧","剧本编剧","剧本编剧"],
-    art: "原画画师",
-    cover: "img/game/0.jpg", // 封面图路径，统一前缀img/
+    name: "时钟机关默示录",
+    year: "2024",
+    publisher: "Otomate",
+    cnStudio: "JOYOLAND",
+    writer: ["雨宮うた","笹川チエ"],
+    art: "花羽彩",
+    cover: "img/game/040.jpg", // 封面图路径，统一前缀img/
     charList: [
-        // 女主模板 female
+        // 拉奇娅·菲利茨
         {
-            id: "g0_f01",
-            name: "女主A",
+            id: "g040_f01",
+            name: "拉奇娅·菲利茨",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Latchia.jpg","img/char/040/Latchia2.jpg","img/char/040/Latchia3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 利亚姆·耶布兰
         {
-            id: "g0_m01",
-            name: "男主1",
+            id: "g040_m01",
+            name: "利亚姆·耶布兰",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Liam.jpg","img/char/040/Liam2.jpg","img/char/040/Liam3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 库厄特·赫尔特林
         {
-            id: "g0_m02",
-            name: "男主1",
+            id: "g040_m02",
+            name: "库厄特·赫尔特林",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Quat.jpg","img/char/040/Quat2.jpg","img/char/040/Quat3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 鲁德尔·克洛伊兹
         {
-            id: "g0_m03",
-            name: "男主1",
+            id: "g040_m03",
+            name: "鲁德尔·克洛伊兹",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Rudel.jpg","img/char/040/Rudel2.jpg","img/char/040/Rudel3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 尤纳卡·基斯贝尔特
         {
-            id: "g0_m04",
-            name: "男主1",
+            id: "g040_m04",
+            name: "尤纳卡·基斯贝尔特",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Unka.jpg","img/char/040/Unka2.jpg","img/char/040/Unka3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 吉尔·哈尼什
         {
-            id: "g0_m05",
-            name: "男主1",
+            id: "g040_m05",
+            name: "吉尔·哈尼什",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/040/Zir.jpg","img/char/040/Zir2.jpg","img/char/040/Zir3.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // 加奈特
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
+            id: "g040_h01",
+            name: "加奈特",
             gender: "male",
             isHidden: true,
             isFD: false,
             images: [
-                { srcList: ["img/char/g001_h01_1.jpg"], type: "base" }
+                { srcList: ["img/char/040/Gannet.jpg"], type: "base" }
             ]
         }
     ]
