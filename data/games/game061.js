41e269d47f334b8bcbaa7d650b82fd8c210f1f96
OtomeFavList <otomefavlist@163.com>
2026-08-10 00:27:43 +0800

    Update game061.js

--- data/games/game061.js
+++ data/games/game061.js
@@ -16,114 +16,114 @@ const gameData = {
     ],
     cover: "img/game/061.jpg", // 封面图路径，统一前缀img/
     charList: [
-        // 女主模板 female
+        // 天近绫乃
         {
             id: "g061_f01",
-            name: "女主A",
+            name: "天近绫乃",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Ayano.jpg","img/char/061/Ayano2.png","img/char/061/Ayano3.jpg","img/char/061/Ayano4.jpg","img/char/061/Ayano5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 女主模板 female
+        // 天近美铃
         {
             id: "g061_f02",
-            name: "女主A",
+            name: "天近美铃",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/g001_f0_1.jpg","img/char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Misuzu.jpg","img/char/061/Misuzu2.png","img/char/061/Misuzu3.jpg","img/char/061/Misuzu4.jpg","img/char/061/Misuzu5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 月城茅
         {
             id: "g061_m01",
-            name: "男主1",
+            name: "月城茅",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Chigaya.jpg","img/char/061/Chigaya2.png","img/char/061/Chigaya3.jpg","img/char/061/Chigaya4.jpg","img/char/061/Chigaya5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 海濑隼人
         {
             id: "g061_m02",
-            name: "男主1",
+            name: "海濑隼人",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Hayato.jpg","img/char/061/Hayato2.png","img/char/061/Hayato3.jpg","img/char/061/Hayato4.jpg","img/char/061/Hayato5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 风见琉佳
         {
             id: "g061_m03",
-            name: "男主1",
+            name: "风见琉佳",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Luka.jpg","img/char/061/Luka2.png","img/char/061/Luka3.jpg","img/char/061/Luka4.jpg","img/char/061/Luka5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 花丘槙尾
         {
             id: "g061_m04",
-            name: "男主1",
+            name: "花丘槙尾",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Makio.jpg","img/char/061/Makio2.png","img/char/061/Makio3.jpg","img/char/061/Makio4.jpg","img/char/061/Makio5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 日向野骏
         {
             id: "g061_m05",
-            name: "男主1",
+            name: "日向野骏",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Shun.jpg","img/char/061/Shun2.png","img/char/061/Shun3.jpg","img/char/061/Shun4.jpg","img/char/061/Shun5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 空闲巴
         {
             id: "g061_m06",
-            name: "男主1",
+            name: "空闲巴",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Tomoe.jpg","img/char/061/Tomoe2.png","img/char/061/Tomoe3.jpg","img/char/061/Tomoe4.jpg","img/char/061/Tomoe5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 月城苇夜
         {
             id: "g061_m07",
-            name: "男主1",
+            name: "月城苇夜",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Yoshiya.jpg","img/char/061/Yoshiya2.png","img/char/061/Yoshiya3.jpg","img/char/061/Yoshiya4.jpg","img/char/061/Yoshiya5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 善知鸟全
         {
             id: "g061_m08",
-            name: "男主1",
+            name: "善知鸟全",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/g0_m01_1.jpg","img/char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["img/char/061/Zen.jpg","img/char/061/Zen2.png","img/char/061/Zen3.jpg","img/char/061/Zen4.jpg","img/char/061/Zen5.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         }
     ]
