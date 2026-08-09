7f4163899993da6d7c566ae4c19ff6d22d17c115
OtomeFavList <otomefavlist@163.com>
2026-08-09 23:20:20 +0800

    Update games.js

--- data/games.js
+++ data/games.js
@@ -13,6 +13,7 @@ const allGameFileNames = [
     "game041.js", "game042.js", "game043.js", "game044.js", "game045.js",
     "game046.js", "game047.js", "game048.js", "game049.js", "game050.js",
     "game051.js", "game052.js", "game053.js", "game054.js", "game055.js",
+    "game056.js", "game057.js", "game058.js", "game059.js", "game060.js",
     // 后续新增游戏仅在此添加一行文件名即可
 ];
 
