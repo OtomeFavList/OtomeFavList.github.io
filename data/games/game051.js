ff1e93b4678e3d2eb9cddce38c9f380956a3517c
OtomeFavList <otomefavlist@163.com>
2026-08-14 03:13:53 +0800

    Update game051.js

--- data/games/game051.js
+++ data/games/game051.js
@@ -15,7 +15,7 @@ const gameData = {
         {name:"佐倉たくと", lang:"zh"},
         {name:"ろく丸", lang:"ja"}
     ],
-    cover: "img/game/051.jpg", // 封面图路径，统一前缀img/
+    cover: "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/game/051.jpg", // 封面图路径，统一前缀img/
     charList: [
         // 小川葵
         {
@@ -25,7 +25,8 @@ const gameData = {
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/051/Aoi.jpg","img/char/051/Aoi2.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Aoi.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Aoi2.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 加贺阳
@@ -36,7 +37,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Haru.jpg","img/char/051/Haru2.jpg","img/char/051/Haru3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Haru.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Haru2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Haru3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 绵森枫
@@ -47,7 +50,8 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Kaede.jpg","img/char/051/Kaede2.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kaede.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kaede2.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 木野濑一辉
@@ -58,7 +62,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Kazuki.jpg","img/char/051/Kazuki2.jpg","img/char/051/Kazuki3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kazuki.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kazuki2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kazuki3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 浅浪皓
@@ -69,7 +75,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Kou.jpg","img/char/051/Kou2.jpg","img/char/051/Kou3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kou.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kou2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Kou3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 篠原涼太
@@ -80,7 +88,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Ryouta.jpg","img/char/051/Ryouta2.jpg","img/char/051/Ryouta3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Ryouta.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Ryouta2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Ryouta3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 泽野井宗介
@@ -91,7 +101,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/051/Sousuke.jpg","img/char/051/Sousuke2.jpg","img/char/051/Sousuke3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Sousuke.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Sousuke2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/051/Sousuke3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         }
     ]
