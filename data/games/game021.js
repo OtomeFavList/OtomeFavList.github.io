a80b040208b242e2ebc974f261330c33a2dda328
OtomeFavList <otomefavlist@163.com>
2026-08-14 01:57:57 +0800

    Update game021.js

--- data/games/game021.js
+++ data/games/game021.js
@@ -14,7 +14,7 @@ const gameData = {
     art: [
         {name:"藤丸豆ノ介", lang:"zh"}
     ],
-    cover: "img/game/021.jpg", // 封面图路径，统一前缀img/
+    cover: "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/game/021.jpg", // 封面图路径，统一前缀img/
     charList: [
         // 爱丽丝·利德尔
         {
@@ -24,7 +24,8 @@ const gameData = {
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/021/Alice.jpg","img/char/021/Alice2.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Alice.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Alice2.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 布拉德·都普雷
@@ -35,7 +36,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Blood.jpg","img/char/021/Blood2.jpg","img/char/021/Blood3.jpg","img/char/021/Blood4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Blood.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Blood2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Blood3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Blood4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 崔德尔·迪
@@ -46,7 +50,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Dee.jpg","img/char/021/Dee2.jpg","img/char/021/Dee3.jpg","img/char/021/Dee4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dee.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dee2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dee3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dee4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 崔德尔·达姆
@@ -57,7 +64,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Dum.jpg","img/char/021/Dum2.jpg","img/char/021/Dum3.jpg","img/char/021/Dum4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dum.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dum2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dum3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Dum4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 艾略特·玛奇
@@ -68,7 +78,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Elliot.jpg","img/char/021/Elliot2.jpg","img/char/021/Elliot3.jpg","img/char/021/Elliot4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Elliot.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Elliot2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Elliot3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Elliot4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 路易斯·可萝
@@ -79,7 +92,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Lewis.jpg","img/char/021/Lewis2.jpg","img/char/021/Lewis3.jpg","img/char/021/Lewis4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Lewis.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Lewis2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Lewis3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Lewis4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 奎恩·希尔凡
@@ -90,7 +106,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/021/Quin.jpg","img/char/021/Quin2.jpg","img/char/021/Quin3.jpg","img/char/021/Quin4.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Quin.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Quin2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Quin3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Quin4.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 艾斯
@@ -101,7 +120,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Ace.jpg","img/char/021/Ace2.jpg","img/char/021/Ace3.jpg","img/char/021/Ace4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Ace.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Ace2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Ace3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Ace4.png"], type: "base" }
             ]
         },
         // 波利斯·艾瑞
@@ -112,7 +134,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Boris.jpg","img/char/021/Boris2.jpg","img/char/021/Boris3.jpg","img/char/021/Boris4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Boris.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Boris2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Boris3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Boris4.png"], type: "base" }
             ]
         },
         // 格雷·林谷马克
@@ -123,7 +148,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Gray.jpg","img/char/021/Gray2.jpg","img/char/021/Gray3.jpg","img/char/021/Gray4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Gray.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Gray2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Gray3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Gray4.png"], type: "base" }
             ]
         },
         // 汉尼瓦·葛特
@@ -134,7 +162,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Hannibal.jpg","img/char/021/Hannibal2.jpg","img/char/021/Hannibal3.jpg","img/char/021/Hannibal4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Hannibal.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Hannibal2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Hannibal3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Hannibal4.png"], type: "base" }
             ]
         },
         // Joker
@@ -145,7 +176,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Joker.jpg","img/char/021/Joker2.jpg","img/char/021/Joker3.jpg","img/char/021/Joker4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Joker.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Joker2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Joker3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Joker4.png"], type: "base" }
             ]
         },
         // 奈特梅尔·哥德夏洛克
@@ -156,7 +190,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/021/Nightmare.jpg","img/char/021/Nightmare2.jpg","img/char/021/Nightmare3.jpg","img/char/021/Nightmare4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Nightmare.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Nightmare2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Nightmare3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/021/Nightmare4.png"], type: "base" }
             ]
         }
     ]
