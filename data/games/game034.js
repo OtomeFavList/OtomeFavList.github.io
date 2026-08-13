7a2bb093bd2f27ad04184ec908c073748b3bed88
OtomeFavList <otomefavlist@163.com>
2026-08-14 02:17:11 +0800

    Update game034.js

--- data/games/game034.js
+++ data/games/game034.js
@@ -13,7 +13,7 @@ const gameData = {
     art: [
         {name:"ne-on", lang:"en"}
     ],
-    cover: "img/game/034.jpg", // 封面图路径，统一前缀img/
+    cover: "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/game/034.jpg", // 封面图路径，统一前缀img/
     charList: [
         // 姐崎希美
         {
@@ -23,7 +23,7 @@ const gameData = {
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/034/Nozomi.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/034/Nozomi.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 仓口步
@@ -34,7 +34,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/034/Ayumu.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/034/Ayumu.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 星名穗积
@@ -45,7 +45,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/034/Hozumi.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/034/Hozumi.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 姐崎隼
@@ -56,7 +56,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/034/Shun.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/034/Shun.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         }
     ]
