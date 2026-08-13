7d1d5af0aa59c8a5b7e888f1c72b3f44348fe38d
OtomeFavList <otomefavlist@163.com>
2026-08-14 02:23:30 +0800

    Update game037.js

--- data/games/game037.js
+++ data/games/game037.js
@@ -15,7 +15,7 @@ const gameData = {
     art: [
         {name:"黒蜜きなこ", lang:"zh"}
     ],
-    cover: "img/game/037.jpg", // 封面图路径，统一前缀img/
+    cover: "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/game/037.jpg", // 封面图路径，统一前缀img/
     charList: [
         // 中山日南子
         {
@@ -25,7 +25,9 @@ const gameData = {
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["img/char/037/Hinako.jpg","img/char/037/Hinako2.jpg","img/char/037/Hinako3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Hinako.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Hinako2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Hinako3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 鬼岛凤凰
@@ -36,7 +38,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/037/Houou.jpg","img/char/037/Houou2.jpg","img/char/037/Houou3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Houou.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Houou2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Houou3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 吉良麟太郎
@@ -47,7 +51,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/037/Rintarou.jpg","img/char/037/Rintarou2.jpg","img/char/037/Rintarou3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Rintarou.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Rintarou2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Rintarou3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 金春贵之
@@ -58,7 +64,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/037/Takayuki.jpg","img/char/037/Takayuki2.jpg","img/char/037/Takayuki3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Takayuki.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Takayuki2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Takayuki3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 箕轮斗斗丸
@@ -69,7 +77,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/037/Totomaru.jpg","img/char/037/Totomaru2.jpg","img/char/037/Totomaru3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Totomaru.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Totomaru2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Totomaru3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 未良子裕太
@@ -80,7 +90,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["img/char/037/Yuuta.jpg","img/char/037/Yuuta2.jpg","img/char/037/Yuuta3.png"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Yuuta.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Yuuta2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Yuuta3.png"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // 相乐天马
@@ -91,7 +103,10 @@ const gameData = {
             isHidden: false,
             isFD: true,
             images: [
-                { srcList: ["img/char/037/Tenma.jpg","img/char/037/Tenma2.jpg","img/char/037/Tenma3.jpg","img/char/037/Tenma4.png"], type: "base" }
+                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Tenma.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Tenma2.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Tenma3.jpg",
+                            "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/037/Tenma4.png"], type: "base" }
             ]
         }
     ]
