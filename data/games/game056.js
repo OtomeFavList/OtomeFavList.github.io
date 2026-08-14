ab7290cd60f619824087c87f0a70ae4faf0db761
OtomeFavList <otomefavlist@163.com>
2026-08-15 03:48:11 +0800

    Update game056.js

--- data/games/game056.js
+++ data/games/game056.js
@@ -13,17 +13,17 @@ const gameData = {
     art: [
         {name:"のりた", lang:"ja"}
     ],
-    cover: "https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/game/056.jpg", // 封面图路径，统一前缀img/
+    cover: "https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/game/056.jpg",
     charList: [
         // 朱丽叶·露斯
         {
             id: "g056_f01",
             name: "朱丽叶·露斯",
             gender: "female",
-            isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
-            isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
+            isHidden: false,
+            isFD: false,
             images: [
-                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/056/Juliet.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/char/056/Juliet.jpg"], type: "base" },
             ]
         },
         // 亚设·汤普森
@@ -34,10 +34,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/056/Asher.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/char/056/Asher.jpg"], type: "base" },
             ]
         },
-        // 普奇兰·洛厄尔
+        // 奇兰·洛厄尔
         {
             id: "g056_m02",
             name: "奇兰·洛厄尔",
@@ -45,7 +45,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/056/Ciaran.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/char/056/Ciaran.jpg"], type: "base" },
             ]
         },
         // 加勒特·威尔金
@@ -56,7 +56,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/056/Garrett.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/char/056/Garrett.jpg"], type: "base" },
             ]
         },
         // 罗德斯·哈特菲尔德
@@ -67,7 +67,7 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["https://pub-7fe3cf5d6e78426b988975ff957a6ee9.r2.dev/char/056/Rhodes.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["https://cdn.jsdelivr.net/gh/OtomeFavList/OtomeFavList.github.io@main/img/char/056/Rhodes.jpg"], type: "base" },
             ]
         }
     ]
