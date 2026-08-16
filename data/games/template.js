c2aace599fb60bbfba8de8efc462b6a71a653ff7
OtomeFavList <otomefavlist@163.com>
2026-08-17 01:21:43 +0800

    Update template.js

--- data/games/template.js
+++ data/games/template.js
@@ -30,10 +30,10 @@ const gameData = {
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["char/001/Liliana.jpg",
-                            "char/g001_f0_2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/g001_f0_3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/g001_f0_4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+                { srcList: ["char/001/女主A.jpg",
+                            "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
+                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
         // 普通可攻略男主 male
@@ -44,10 +44,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/001/Dante.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/001/男主1.jpg",
+                            "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
+                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
+                { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
         // 普通可攻略男主 male
