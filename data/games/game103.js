636b54442d150ee2691ae2ca711f96ebfca769d4
OtomeFavList <otomefavlist@163.com>
2026-08-19 22:18:16 +0800

    Create game103.js

---
+++ data/games/game103.js
@@ -0,0 +1,29 @@
+月影の鎖 - 錯乱パラノイア -
+中文名: 月影之锁 错乱偏执狂
+开发: TAKUYO
+发行: TAKUYO
+人物设定: ヒロセアヅミ
+主题歌作曲: Morrigan、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
+主题歌作词: 片霧烈火、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
+主题歌演出: 片霧烈火、ラムソアーズ（Nintendo Switch版）
+原画: ヒロセアヅミ
+剧本: 白鳥ユアン、井上愁、其他
+音乐: 森藤晶司
+链接: ErogameScape VNDB
+官方网站: takuyo.co.jp/products/getsueifd/getsueifdvita/index.html
+
+开发: TAKUYO
+发行: TAKUYO
+剧本: 白鳥ユアン（共通、大井川、榛名线，猪口线部分）／藤元（藤文）（神乐坂线、望月神乐坂共通）／井上愁（望月线，猪口线部分）／犬小屋煙突
+音乐: 森藤晶司
+人物设定: ヒロセアヅミ
+主题歌作曲: Morrigan
+主题歌作词: 片霧烈火
+主题歌演出: 片霧烈火
+原画: ヒロセアヅミ
+平台: PSP
+游戏类型: 殺伐とした世界の中で一筋の光を見付ける恋愛ADV
+游玩人数: 1
+发行日期: 2013-04-18
+售价: 6,380 円（税込）
+官方网站: takuyo.co.jp/products/getsuei/index.html
