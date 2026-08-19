11c27e9b8c66fe0eb9f7c4930d9f81a0cd7237ec
OtomeFavList <otomefavlist@163.com>
2026-08-19 22:26:53 +0800

    Create game106.js

---
+++ data/games/game106.js
@@ -0,0 +1,32 @@
+ときめきメモリアル Girl's Side 2nd Kiss
+中文名: 心跳回忆女生版2
+开发: KONAMI
+发行: KONAMI
+人物设定: 小松原里枝子
+导演: 内田明理
+别名: ときメモ GS2
+GS2
+ときめきメモリアル Girl’s Side 2nd Season
+ときめきメモリアル Girl’s Side 2nd Season for Nintendo Switch
+平台:
+PS2 NDS Nintendo Switch
+游戏类型: 学園恋愛シミュレーション
+游玩人数: 1
+发行日期:
+PS2初回生産版 2006-08-03
+PS2通常版 2006-08-03
+PS2特別版 2006-08-07
+NDS版 2008-02-14
+Nintendo Switch版 2024-02-14
+售价:
+PS2初回生産版 7,678 円（税込）
+PS2通常版 7,678 円（税込）
+PS2特別版 10,780 円（税込）
+NDS版 7,128 円（税込）
+Nintendo Switch版 5,995 円（税込）
+主题歌作曲: スターダストレビュー、大浦祐一、根本要
+主题歌作词: 大浦祐一、根本要
+主题歌演出: スターダストレビュー、大浦祐一
+链接: ErogameScape VNDB
+官方网站: konami.com/game/Girls_Side/2nd_Season/
+  
