06a402aaa37f83a8d5dae209efeed07a32ec3758
OtomeFavList <otomefavlist@163.com>
2026-08-19 21:35:18 +0800

    Create game091.js

---
+++ data/games/game091.js
@@ -0,0 +1,34 @@
+Blackish House ←sideZ -Retour-
+开发: honeybee black
+发行: アリスマティック、エディア
+人物设定: カズアキ
+平台: Nintendo Switch
+游戏类型: ダメ人間カウンセリング恋愛ADV
+游玩人数: 1
+发行日期: 2026-06-25
+售价:
+通常版 8,580円（税込)
+特装版 14,080円（税込）
+链接: blackishhouse-edia.com/sidez/
+官方网站: honeybee-cd.com/BlackishHouse/switch_sideZ/
+
+Blackish House ←sideZ
+开发: honeybee black
+发行: honeybee black
+剧本: 中村幸代（Noa、Sera线）高木彩佳（悠翔线）坂本美香（那由多线）、もち大福、株式会社エッジワークス（浅生柚子、相川暁子、春名佳純、葉山いずみ、深瀬カエル、郡具もえ、浅野エミイ、日野光里、芝原三恵子、佐久間yaya）
+人物设定: カズアキ
+主题歌作曲: あるるかん
+主题歌作词: あるるかん
+主题歌演出: 蒼井翔太
+原画: カズアキ
+导演: 中村幸代（共通、公演部分）
+平台: PC
+游戏类型: ダメ人間カウンセリング恋愛ADV
+游玩人数: 1
+发行日期: 2017-04-28
+售价:
+通常版 5,800 円＋税
+初回限定版 7,800 円＋税
+SDキャラクター: 三月リヒト
+链接: ErogameScape VNDB
+官方网站: honeybee-cd.com/BlackishHouse/sideZ/
