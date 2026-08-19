638ede2653e6d7ad82d20eb246d8924096cb8968
OtomeFavList <otomefavlist@163.com>
2026-08-19 22:11:11 +0800

    Create game100.js

---
+++ data/games/game100.js
@@ -0,0 +1,15 @@
+sins of KALEIDO 塔巡りし因果の魔女
+开发: Voltage, Inc.、AmuLit
+发行: Voltage, Inc.
+剧本: 潮文音
+音乐: 浅野隼人
+主题歌演出: DUSTCELL
+导演: 潮文音
+企画: Voltage, Inc.
+平台: Nintendo Switch
+游玩人数: 1人
+发行日期: 2026-7-16
+原画: Tana Khaki
+服装设计: ウニコ
+背景: 清井fen
+官方网站: products.voltage.co.jp/kaleido/
