af4c2b51732ffa0190be5423cce8b999b5f6ff66
OtomeFavList <otomefavlist@163.com>
2026-08-19 22:05:56 +0800

    Create game097.js

---
+++ data/games/game097.js
@@ -0,0 +1,66 @@
+Starry☆Sky～Spring Memories～
+开发: honeybee
+发行: dramatic create（ヒューネックス株式会社）
+音乐: 野島健太郎
+人物设定: カズアキ
+原画: カズアキ
+原作: honeybee
+SD原画: ゑむ
+平台: Nintendo Switch
+游戏类型: 春の夜空を幼なじみと見上げる学園ADV
+游玩人数: 1
+发行日期: 2026-05-28
+官方网站: honeybee-cd.com/starrysky/switch_ss_spring/
+
+Starry☆Sky〜in Spring〜
+中文名: 星座彼氏-春季篇
+开发: honeybee
+发行: honeybee
+剧本: 高木聖子、桜葉ユウ、清水慶、株式会社エッジワークス
+人物设定: カズアキ
+主题歌作曲: あるるかん
+主题歌作词: あるるかん
+原画: カズアキ
+导演: 中村幸代
+http://www.honeybee-cd.com/spring/index.html
+
+Starry☆Sky ～in Spring～ ポータブル
+开发: honeybee
+发行: honeybee
+剧本: 高木聖子
+人物设定: カズアキ
+主题歌作曲: 綾部健三郎、上野浩司
+主题歌作词: Lynn
+主题歌演出: いとうかなこ
+原画: カズアキ
+平台:
+PSP PSV
+游玩人数: 1
+发行日期: 2010-06-24
+售价:
+通常版 3,278 円（税込）
+限定版 7,480 円（税込）
+官方网站: honeybee-cd.com/spring-p/
+
+Starry☆Sky〜After Spring〜
+开发: honeybee
+发行: honeybee
+剧本: 株式会社エッジワークス（春名佳純、浅生柚子）
+人物设定: カズアキ
+主题歌作曲: あるるかん、antistar
+主题歌作词: あるるかん
+主题歌演出: 緑川光、antistar
+原画: カズアキ
+http://www.honeybee-cd.com/fan-spring/
+
+Starry☆Sky ~Spring Stories~
+开发: honeybee
+发行: honeybee
+主题歌演出: 緑川光
+平台: PSV
+游玩人数: 1
+发行日期: 2016-11-23
+售价:
+パッケージ版 6,380 円（税込）
+ダウンロード版 5800 円＋税
+官方网站: honeybee-cd.com/starrysky/vita/spring/index.html
