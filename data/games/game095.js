3104797e3a3ea4db3995b0e40404fde8da4e92c8
OtomeFavList <otomefavlist@163.com>
2026-08-19 21:44:26 +0800

    Create game095.js

---
+++ data/games/game095.js
@@ -0,0 +1,63 @@
+DYNAMIC CHORD feat.KYOHSO Remaster Edition
+开发: honeybee black
+发行: dramatic create、アリスマティック
+人物设定: 冨士原良
+主题歌演出: 森久保祥太郎
+原画: 冨士原良
+平台: Nintendo Switch
+游戏类型: 甘く激しく魂震えるバンド恋愛ADV
+游玩人数: 1
+发行日期: 2025-11-27
+售价: 7,480 円（税込）
+官方网站: honeybee-cd.com/dynamic/switch-kyohso/index.html
+
+DYNAMIC CHORD feat.KYOHSO
+开发: honeybee black
+发行: honeybee black
+剧本: Plot：石川奈津季、中村幸代（なかむらさちよ）；执笔：柚子みかん（なかむらさちよ）、さつき、株式会社エッジワークス（浅生柚子；相川暁子、春名佳純、水井としえ；みなづきともこ、ふくだりょうこ、伊原恵）
+人物设定: 冨士原良
+主题歌演出: 森久保祥太郎
+原画: 冨士原良
+平台: PC
+游戏类型: 甘く激しく魂震えるバンド恋愛ADV
+游玩人数: 1
+发行日期: 2015-06-26
+售价:
+通常版 4,104 円
+初回限定A盤 6,264 円
+初回限定B盤 5,724 円
+链接: ErogameScape VNDB
+官方网站: honeybee-cd.com/dynamic/kyoh/index.html
+
+DYNAMIC CHORD feat.KYOHSO Append Disc
+开发: honeybee black
+发行: honeybee black
+人物设定: 冨士原良
+主题歌演出: 森久保祥太郎
+原画: 冨士原良
+平台: PC
+游玩人数: 1
+发行日期:
+初回限定版 2016-07-29
+通常版 2016-07-29
+售价:
+初回限定版 5,830 円（税込）
+通常版 4,180 円（税込）
+剧本: 相川暁子
+官方网站: honeybee-cd.com/dynamic/kyoh/index.html
+
+DYNAMIC CHORD feat.KYOHSO V edition
+开发: honeybee black
+发行: honeybee black
+人物设定: 冨士原良
+主题歌演出: 森久保祥太郎
+原画: 冨士原良
+导演: 中澤工
+平台: PSV
+游戏类型: 甘く激しく魂震えるバンド恋愛ADV
+游玩人数: 1
+发行日期: 2017-03-30
+售价:
+通常版 5,800円＋税
+限定版 7,800円＋税
+官方网站: honeybee-cd.com/dynamic/vita-kyoh/index.html
