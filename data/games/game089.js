16eba43b3640a830e67bd733a0c2ca57765b0db0
OtomeFavList <otomefavlist@163.com>
2026-08-19 21:24:16 +0800

    Create game089.js

---
+++ data/games/game089.js
@@ -0,0 +1,16 @@
+DIG-ROCK -Documentary of Youthful Sounds-
+开发: ALTERGEAR、デザインファクトリー
+发行: アイディアファクトリー
+剧本: 関涼子
+原作: MintLip
+平台: Nintendo Switch
+游戏类型: ドキュメンタリーADV
+发行日期: 2025-08-28
+售价:
+通常版 8,580円（税込）
+特装版 10,780円（税込）
+ＤＬ版 8,580円（税込）
+导演: General Director: 久保嶋愛香（ティームエンタテインメント）、Game Director: 椎名あとり
+人物设定: 真久
+原画: 猫井アユ（Impish Crow）、六月ロク（RUBIA Leopard）
+官方网站: ideaf.co.jp/altergear/dig-rock/
