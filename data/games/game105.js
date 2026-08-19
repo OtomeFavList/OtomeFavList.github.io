dad789f0c7d95ec9abf9dd581fbae7ae7968c59e
OtomeFavList <otomefavlist@163.com>
2026-08-19 22:24:29 +0800

    Create game105.js

---
+++ data/games/game105.js
@@ -0,0 +1,43 @@
+ときめきメモリアル Girl's Side 1st Love Plus
+中文名: 心跳回忆女生版1 加强版
+开发: KONAMI
+发行: KONAMI
+人物设定: 小松原里枝子
+别名: ときめきメモリアル Girl’s Side 1st Love for Nintendo Switch
+平台:
+NDS Nintendo Switch
+游戏类型: 恋爱养成
+游玩人数: 1
+发行日期:
+NDS版 2009-03-12
+Nintendo Switch版 2024-02-14
+售价:
+NDS版 5,500 円（税込）
+Nintendo Switch版 5,995 円（税込）
+官方网站: konami.jp/gs/game/Girls_Side/1st_Love/
+
+ときめきメモリアル Girl's Side
+中文名: 心跳回忆女生版
+开发: KONAMI
+发行: KONAMI
+剧本: 内田明理、子安秀明、山田桜丸、成田伸子、春日直登、栗原秀和、芳野未来
+人物设定: 小松原里枝子
+主题歌作曲: CUBE、松本孝弘
+主题歌作词: 稲葉浩志
+主题歌演出: B'z、B'z
+平台: PS2
+游戏类型: 恋爱养成游戏
+游玩人数: 1
+发行日期:
+初回限定版 2002-06-20
+通常版 2002-06-20
+コナミ ザ・ベスト版 2003-09-18
+コナミ殿堂セレクション版 2004-10-21
+售价:
+初回限定版 7,480 円（税込）
+通常版 7,480 円（税込）
+コナミ ザ・ベスト版 3,080 円（税込）
+コナミ殿堂セレクション版 1,980 円（税込）
+音乐: CUBE(株式会社キューブ)
+链接: ErogameScape VNDB
+官方网站: konami.jp/gs/game/Girls_Side/
