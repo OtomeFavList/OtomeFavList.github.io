51eb7fe86154fafffeeff9fb3ce2d154188dcc9b
OtomeFavList <otomefavlist@163.com>
2026-08-19 21:53:52 +0800

    Create game096.js

---
+++ data/games/game096.js
@@ -0,0 +1,25 @@
+闇色の魔珠
+中文名: 暗色的魔珠
+开发: Daisy²
+发行: Daisy²、株式会社プロトタイプ
+剧本: 雨宮うた
+音乐: 細井聡司(細井そうし)
+人物设定: めろ
+主题歌演出: SUZUKA、YURIA、中原涼
+原画: めろ
+平台:
+PC Nintendo Switch
+游戏类型: 全年齡乙女ゲーム
+游玩人数: 1
+发行日期:
+PC版 2023-12-22
+Nintendo Switch版 2025-11-20
+售价:
+PC豪華特装版 11,000円（税込）
+PC通常版 8,250円（税込）
+Nintendo Switchパッケージ通常版 6,820 円（税込）
+Nintendo Switchパッケージ初回限定版 11,880 円（税込）
+Nintendo Switchダウンロード版 5,800 円（税込）
+主题歌作曲: 細井そうし
+链接: ErogameScape VNDB
+官方网站: daisy-daisy.in/products/majyu/
