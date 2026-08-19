bcdd36b3132caee808795055eb49821e24c512fb
OtomeFavList <otomefavlist@163.com>
2026-08-19 20:33:59 +0800

    Create game077.js

---
+++ data/games/game077.js
@@ -0,0 +1,23 @@
+中文名: DYNAMIC CHORD 动态和弦 feat.Liar-S Remaster edition
+开发: honeybee black
+发行: アリスマティック、dramatic create、欢乐百世
+人物设定: 冨士原良
+原画: 冨士原良
+别名: ダイナミックコード フィーチャリング ライアーズ リマスター エディション
+honeybee-cd.com/dynamic/switch-liar/
+
+DYNAMIC CHORD feat.Liar-S
+剧本: Plot：高木彩佳；执笔：もち大福、株式会社エッジワークス（浅生柚子；相川暁子、森田彩莉、桜ゆづき、葉山いずみ）
+honeybee-cd.com/dynamic/liar/index.html
+
+DYNAMIC CHORD feat.Liar-S Append Disc
+剧本: 桜ゆづき、相川暁子、葉山いずみ
+honeybee-cd.com/dynamic/liar/append.html
+
+DYNAMIC CHORD feat.Liar-S V edition
+开发: honeybee black
+发行: honeybee black
+剧本: もち大福、株式会社エッジワークス、桜 ゆづき、森田彩莉、浅生柚子、相川暁子、葉山いずみ
+人物设定: 冨士原良
+原画: 冨士原良
+honeybee-cd.com/dynamic/vita-liar/index.html
