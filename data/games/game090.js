e142d43d0b2e17ea3795ff6a1506b6a58a84a85e
OtomeFavList <otomefavlist@163.com>
2026-08-19 21:33:35 +0800

    Create game090.js

---
+++ data/games/game090.js
@@ -0,0 +1,26 @@
+Blackish House sideA→ -Retour-
+开发: honeybee black
+发行: アリスマティック、エディア
+人物设定: カズアキ
+平台: Nintendo Switch
+官方网站: honeybee-cd.com/BlackishHouse/switch_sideA/
+
+Blackish House sideA→
+开发: honeybee black
+发行: honeybee black
+剧本: 中村幸代（円线）、みなづきともこ、ゆずみやともめ、乙月ありさ、伊原恵、桜ゆづき、柿本悠理、煌矢まな、谷村知子
+人物设定: カズアキ
+主题歌作曲: あるるかん
+主题歌作词: あるるかん
+主题歌演出: 石川界人
+插入歌演出: 蒼井翔太
+原画: カズアキ
+导演: 高木彩佳（共通、公演部分、其它3线）
+平台: PC
+游戏类型: ダメ人間カウンセリング恋愛ADV
+游玩人数: 1
+发行日期: 2016-08-26
+售价:
+通常版 6,264 円
+初回限定版 8,424 円
+官方网站: honeybee-cd.com/BlackishHouse/sideA/
