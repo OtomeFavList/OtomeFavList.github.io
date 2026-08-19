9be33a013136800ae71f71ac31167a2ffd4f6616
OtomeFavList <otomefavlist@163.com>
2026-08-19 20:39:01 +0800

    Create game083.js

---
+++ data/games/game083.js
@@ -0,0 +1,12 @@
+Panic Palette
+中文名: 恐慌调色盘
+开发: TAKUYO
+发行: TAKUYO
+剧本: 井上愁、藤元（藤文）
+人物设定: 仁神ユキタカ
+主题歌作曲: 森藤晶司、ざっとん、カケリネ
+主题歌作词: CAO、アキレスKEN、カケリネ
+主题歌演出: flaque、ラムソアーズ
+原画: 仁神ユキタカ
+别名: Panic Palette~パニック パレット~
+takuyo.co.jp/products/panipare/
