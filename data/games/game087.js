9c23adb83e86334f65a5e87aaf5f43c91ac8316c
OtomeFavList <otomefavlist@163.com>
2026-08-19 20:41:42 +0800

    Create game087.js

---
+++ data/games/game087.js
@@ -0,0 +1,12 @@
+DistortedCode -生者の残り香-
+开发: TAKUYO
+发行: TAKUYO
+人物设定: あとりちや
+原画: あとりちや
+导演: ちひろ、関口琴子
+别名: ディストーテッドコード
+平台: Nintendo Switch
+游戏类型: 乙女向けホラーアドベンチャー
+剧本: ちひろ
+企画: ちひろ
+takuyo.co.jp/products/distortedcode/index.html
