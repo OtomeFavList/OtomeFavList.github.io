0ca3c4294ae1e5abce8a24e81570af2b2766276c
OtomeFavList <otomefavlist@163.com>
2026-08-19 20:39:30 +0800

    Create game084.js

---
+++ data/games/game084.js
@@ -0,0 +1,15 @@
+魔女と亡霊のヴォロンテ
+开发: LocaGames
+发行: LocaGames
+主题歌作曲: 鈴葉ユミ
+主题歌作词: 鈴葉ユミ
+主题歌演出: 鈴葉ユミ
+企画: LocaGames
+平台: Nintendo Switch
+游戏类型: 悲劇の真相を追求する、ダークファンタジー×サスペンス乙女ゲーム
+游玩人数: 1
+发行日期: 2025-05-22
+售价: 6,930 円(税込)
+原画: Somate Studio
+人物设定: Somate Studio
+loca.games/volontes/
