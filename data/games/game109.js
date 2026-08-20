6e2647681aa48f9e9a698510414910dce4827fb0
OtomeFavList <otomefavlist@163.com>
2026-08-20 09:15:07 +0800

    Create game109.js

---
+++ data/games/game109.js
@@ -0,0 +1,46 @@
+BLACK WOLVES SAGA -Weiβ und Schwarz- for Nintendo Switch
+
+
+BLACK WOLVES SAGA -Weiβ und Schwarz-
+开发: Rejet、オトメイト
+发行: Rejet、アイディアファクトリー株式会社、オトメイト
+主题歌演出: lasah
+原画: 黒裄
+原作: Rejet
+别名: BLACK WOLVES SAGA -Weiβ und Schwarz- for Nintendo Switch
+平台:
+Nintendo Switch PSV
+官方网站: otomate.jp/bws/wus/
+  
+BLACK WOLVES SAGA -Bloody Nightmare-
+中文名: 黑狼传说 血色梦魇
+开发: Rejet
+发行: Rejet
+音乐: 光田康典、土屋俊輔、亀岡夏海
+人物设定: 黒裄
+主题歌作曲: 光田康典、亀岡夏海
+主题歌作词: 岩崎大介
+主题歌演出: lasah
+插入歌演出: lasah、梶裕貴
+原画: 黒裄
+程序: 細谷博子
+剧本: 山田かのこ
+插入歌作曲: 土屋俊輔
+插入歌作词: 土屋俊輔
+导演: 伊東紗希
+链接: ErogameScape VNDB
+官方网站: rejetweb.jp/bws/top/
+  
+BLACK WOLVES SAGA -Last Hope-
+开发: オトメイト、Rejet
+发行: オトメイト、アイディアファクトリー株式会社
+剧本: 山田かのこ、SOM、砂原有季
+音乐: 土屋俊輔、光田康典
+人物设定: 黒裄
+主题歌作曲: 土屋俊輔、亀岡夏海
+主题歌作词: lasah、岩崎大介、亀岡夏海
+主题歌演出: 梶裕貴、lasah
+插入歌演出: lasah
+原画: 黒裄
+导演: 伊東紗希
+官方网站: rejetweb.jp/bw
