ab9c198b27fd183fc1859ba48993d24e520229ae
OtomeFavList <otomefavlist@163.com>
2026-08-22 05:43:16 +0800

    Update game081.js

--- data/games/game081.js
+++ data/games/game081.js
@@ -19,105 +19,89 @@
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game081", // 全局唯一ID，不可重复，如 game001
+    name: "トラブル・マギア ～訳アリ少女は未来を勝ち取るために異国の魔法学校へ留学します～",
+    year: "2024",
+    publisher: ["Otomate"],
+    cnStudio: "暂无",
     writer: [
-        {name:"中文编剧", lang:"zh"},
-        {name:"日文编剧", lang:"ja"},
-        {name:"英文编剧", lang:"en"}
+        {name:"夏野景", lang:"zh"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"鈴ノ助", lang:"zh"}
     ],
-    cover: "game/0.jpg", // 相对路径，游戏封面
+    cover: "game/081.jpg", // 相对路径，游戏封面
     charList: [
-        // ルチア・ユエン
+        // ルチア·ユエン
         {
-            id: "g0_f01",
-            name: "ルチア・ユエン",
+            id: "g081_f01",
+            name: "ルチア·ユエン",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
                 { srcList: ["char/001/女主A.jpg",
                             "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // アレクセイ・エルディール
+        // アレクセイ·エルディール
         {
-            id: "g0_m01",
-            name: "アレクセイ・エルディール",
+            id: "g081_m01",
+            name: "アレクセイ·エルディール",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/001/男主1.jpg",
                             "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // イザヤ・クライン
+        // イザヤ·クライン
         {
-            id: "g0_m02",
-            name: "イザヤ・クライン",
+            id: "g081_m02",
+            name: "イザヤ·クライン",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // ラルス・クロッツ
+        // ラルス·クロッツ
         {
-            id: "g0_m03",
-            name: "ラルス・クロッツ",
+            id: "g081_m03",
+            name: "ラルス·クロッツ",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // リカルド・サンテ
+        // リカルド·サンテ
         {
-            id: "g0_m04",
-            name: "リカルド・サンテ",
+            id: "g081_m04",
+            name: "リカルド·サンテ",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普シオン・カルド
+        // 普シオン·カルド
         {
-            id: "g0_m05",
-            name: "シオン・カルド",
+            id: "g081_m05",
+            name: "シオン·カルド",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         }
     ]
