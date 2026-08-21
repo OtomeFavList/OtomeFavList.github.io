210eaca6675e722b5965bdf6e70bf37c99b94ab3
OtomeFavList <otomefavlist@163.com>
2026-08-22 04:58:37 +0800

    Update game083.js

--- data/games/game083.js
+++ data/games/game083.js
@@ -1,15 +1,15 @@
-Panic Palette
-中文名: 恐慌调色盘
-开发: TAKUYO
-发行: TAKUYO
-剧本: 井上愁、藤元（藤文）
-人物设定: 仁神ユキタカ
-主题歌作曲: 森藤晶司、ざっとん、カケリネ
-主题歌作词: CAO、アキレスKEN、カケリネ
-主题歌演出: flaque、ラムソアーズ
-原画: 仁神ユキタカ
-别名: Panic Palette~パニック パレット~
-takuyo.co.jp/products/panipare/
+// Panic Palette
+// 中文名: 恐慌调色盘
+// 开发: TAKUYO
+// 发行: TAKUYO
+// 剧本: 井上愁、藤元（藤文）
+// 人物设定: 仁神ユキタカ
+// 主题歌作曲: 森藤晶司、ざっとん、カケリネ
+// 主题歌作词: CAO、アキレスKEN、カケリネ
+// 主题歌演出: flaque、ラムソアーズ
+// 原画: 仁神ユキタカ
+// 别名: Panic Palette~パニック パレット~
+// takuyo.co.jp/products/panipare/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -36,10 +36,10 @@ const gameData = {
     ],
     cover: "game/0.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 依藤亜貴
         {
             id: "g0_f01",
-            name: "女主A",
+            name: "依藤亜貴",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
@@ -50,10 +50,10 @@ const gameData = {
                 { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 普通可攻略男主 male
+        // 烏羽一徒
         {
             id: "g0_m01",
-            name: "男主1",
+            name: "烏羽一徒",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -64,10 +64,10 @@ const gameData = {
                 { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 真朱柑
         {
             id: "g0_m02",
-            name: "男主2",
+            name: "真朱柑",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -78,10 +78,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 乃凪範尚
         {
             id: "g0_m03",
-            name: "男主3",
+            name: "乃凪範尚",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -92,10 +92,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 内沼葛
         {
             id: "g0_m04",
-            name: "男主4",
+            name: "内沼葛",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -106,10 +106,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 白原尋也
         {
             id: "g0_m05",
-            name: "男主5",
+            name: "白原尋也",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -120,26 +120,18 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // 黄朽葉宰
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
+            id: "g0_m06",
+            name: "黄朽葉宰",
             gender: "male",
             isHidden: false,
-            isFD: true,
+            isFD: false,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/g0_m01_1.jpg",
+                            "char/g001_m0_2.jpg"], type: "base" },
+                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
+                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         }
     ]
