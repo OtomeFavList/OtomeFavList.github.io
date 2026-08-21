80d4fc93719549ddfbab3869cb079570db9ce544
OtomeFavList <otomefavlist@163.com>
2026-08-21 14:33:33 +0800

    Create game109.js

---
+++ data/games/game109.js
@@ -0,0 +1,46 @@
+BROTHERS CONFLICT Precious Baby
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+原画: ウダジョ
+别名: BROTHERS CONFLICT Precious Baby for Nintendo Switch
+ブラザース コンフリクト プレシャス ベイビー
+剧本: 小林悠奈、川澄あみ
+企画: 叶瀬あつこ
+原案: 叶瀬あつこ
+链接: ErogameScape VNDB
+官方网站: otomate.jp/bc/pb/
+
+BROTHERS CONFLICT  Passion Pink
+中文名: 兄弟战争 激情粉红
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+主题歌作曲: 小山哉枝、小野貴光、河合英嗣
+主题歌演出: 鈴村健一、鳥海浩輔
+原画: ウダジョ
+企画: 叶瀬あつこ
+主题歌作词: 小山哉枝
+官方网站: otomate.jp/bc/
+
+BROTHERS CONFLICT Brilliant Blue
+中文名: 兄弟战争 闪亮蔚蓝
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+主题歌作曲: L'aide、吉田和人
+主题歌演出: 前野智昭、鈴村健一、鳥海浩輔
+原画: ウダジョ
+平台: PSP
+游戏类型: ちょっとケンカなキョ―ダイ間恋愛SLG
+游玩人数: 1
+发行日期: 2013-09-12
+售价:
+通常版 6,380 円（税込）
+限定版 8,580 円（税込）
+企画: 叶瀬あつこ
+主题歌作词: 小山哉枝
+官方网站: otomate.jp/bc/
