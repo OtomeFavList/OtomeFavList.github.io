fb7e8e5b3a81d8741b49650eb4817a4d4eba0006
OtomeFavList <otomefavlist@163.com>
2026-08-22 05:00:46 +0800

    Update game084.js

--- data/games/game084.js
+++ data/games/game084.js
@@ -1,18 +1,18 @@
-魔女と亡霊のヴォロンテ
-开发: LocaGames
-发行: LocaGames
-主题歌作曲: 鈴葉ユミ
-主题歌作词: 鈴葉ユミ
-主题歌演出: 鈴葉ユミ
-企画: LocaGames
-平台: Nintendo Switch
-游戏类型: 悲劇の真相を追求する、ダークファンタジー×サスペンス乙女ゲーム
-游玩人数: 1
-发行日期: 2025-05-22
-售价: 6,930 円(税込)
-原画: Somate Studio
-人物设定: Somate Studio
-loca.games/volontes/
+// 魔女と亡霊のヴォロンテ
+// 开发: LocaGames
+// 发行: LocaGames
+// 主题歌作曲: 鈴葉ユミ
+// 主题歌作词: 鈴葉ユミ
+// 主题歌演出: 鈴葉ユミ
+// 企画: LocaGames
+// 平台: Nintendo Switch
+// 游戏类型: 悲劇の真相を追求する、ダークファンタジー×サスペンス乙女ゲーム
+// 游玩人数: 1
+// 发行日期: 2025-05-22
+// 售价: 6,930 円(税込)
+// 原画: Somate Studio
+// 人物设定: Somate Studio
+// loca.games/volontes/
   
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -39,10 +39,10 @@ const gameData = {
     ],
     cover: "game/0.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // フィエナ
         {
             id: "g0_f01",
-            name: "女主A",
+            name: "フィエナ",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
@@ -53,10 +53,10 @@ const gameData = {
                 { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 普通可攻略男主 male
+        // エマニュエル・ド・ボーモン
         {
             id: "g0_m01",
-            name: "男主1",
+            name: "エマニュエル・ド・ボーモン",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -67,10 +67,10 @@ const gameData = {
                 { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // イスマイール
         {
             id: "g0_m02",
-            name: "男主2",
+            name: "イスマイール",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -81,10 +81,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // メロディー
         {
             id: "g0_m03",
-            name: "男主3",
+            name: "メロディー",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -95,10 +95,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // オリヴィエ・パケ
         {
             id: "g0_m04",
-            name: "男主4",
+            name: "オリヴィエ・パケ",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -107,42 +107,6 @@ const gameData = {
                             "char/g001_m0_2.jpg"], type: "base" },
                 { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 普通可攻略男主 male
-        {
-            id: "g0_m05",
-            name: "男主5",
-            gender: "male",
-            isHidden: false,
-            isFD: false,
-            images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
-        {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
             ]
         }
     ]
