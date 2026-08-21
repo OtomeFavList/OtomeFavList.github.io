4b1e3fc4957aca6d870a63cc0571a896b31be6d6
OtomeFavList <otomefavlist@163.com>
2026-08-22 05:22:39 +0800

    Update game084.js

--- data/games/game084.js
+++ data/games/game084.js
@@ -22,91 +22,72 @@
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game084", // 全局唯一ID，不可重复，如 game001
+    name: "魔女と亡霊のヴォロンテ",
+    year: "2025",
+    publisher: ["LocaGames"],
+    cnStudio: "暂无",
     writer: [
-        {name:"中文编剧", lang:"zh"},
-        {name:"日文编剧", lang:"ja"},
-        {name:"英文编剧", lang:"en"}
+        {name:"Yulius", lang:"en"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"Somate Studio", lang:"en"}
     ],
-    cover: "game/0.jpg", // 相对路径，游戏封面
+    cover: "game/084.jpg", // 相对路径，游戏封面
     charList: [
         // フィエナ
         {
-            id: "g0_f01",
+            id: "g084_f01",
             name: "フィエナ",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["char/001/女主A.jpg",
-                            "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+                { srcList: ["char/001/女主A.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // エマニュエル・ド・ボーモン
+        // エマニュエル·ド・ボーモン
         {
-            id: "g0_m01",
-            name: "エマニュエル・ド・ボーモン",
+            id: "g084_m01",
+            name: "エマニュエル·ド・ボーモン",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/001/男主1.jpg",
-                            "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
+                { srcList: ["char/001/男主1.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
         // イスマイール
         {
-            id: "g0_m02",
+            id: "g084_m02",
             name: "イスマイール",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
         // メロディー
         {
-            id: "g0_m03",
+            id: "g084_m03",
             name: "メロディー",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // オリヴィエ・パケ
+        // オリヴィエ·パケ
         {
-            id: "g0_m04",
-            name: "オリヴィエ・パケ",
+            id: "g084_m04",
+            name: "オリヴィエ·パケ",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         }
     ]
