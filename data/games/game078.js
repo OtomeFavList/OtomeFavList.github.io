fda03ad2e3f704cebe0dcc571a067fb6d2451bcb
OtomeFavList <otomefavlist@163.com>
2026-08-22 04:24:02 +0800

    Update game078.js

--- data/games/game078.js
+++ data/games/game078.js
@@ -1,21 +1,21 @@
-ネオンクラッシュ -Echoes of the Lost-
-中文名: 霓虹三国 -Echoes of the Lost-
-开发: Voltage, Inc.、AmuLit
-发行: Voltage, Inc.、欢乐百世
-剧本: 吉村りりか
-音乐: 土屋俊輔
-主题歌演出: 平田志穂子
-企画: Voltage, Inc.、野原悟史
-制作人: 野原悟史
-别名: ネオンクラッシュ
-平台: Nintendo Switch
-游戏类型: 女性向けダークファンタジーADV
-游玩人数: 1人
-发行日期: 2025-12-11
-售价: 8,140円
-导演: 椎名一叶（ボルテージ）
-原画: 多花緒（ボルテージ）
-products.voltage.co.jp/neonclash/
+// ネオンクラッシュ -Echoes of the Lost-
+// 中文名: 霓虹三国 -Echoes of the Lost-
+// 开发: Voltage, Inc.、AmuLit
+// 发行: Voltage, Inc.、欢乐百世
+// 剧本: 吉村りりか
+// 音乐: 土屋俊輔
+// 主题歌演出: 平田志穂子
+// 企画: Voltage, Inc.、野原悟史
+// 制作人: 野原悟史
+// 别名: ネオンクラッシュ
+// 平台: Nintendo Switch
+// 游戏类型: 女性向けダークファンタジーADV
+// 游玩人数: 1人
+// 发行日期: 2025-12-11
+// 售价: 8,140円
+// 导演: 椎名一叶（ボルテージ）
+// 原画: 多花緒（ボルテージ）
+// products.voltage.co.jp/neonclash/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
