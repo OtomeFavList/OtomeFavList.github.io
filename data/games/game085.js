8def7664bf370ae3cc5b98ee159edf6cd2df6e63
OtomeFavList <otomefavlist@163.com>
2026-08-22 05:10:36 +0800

    Update game085.js

--- data/games/game085.js
+++ data/games/game085.js
@@ -1,11 +1,11 @@
-BYAKKO ～四神部隊炎恋記～
-开发: オトメイト
-发行: アイディアファクトリー株式会社、オトメイト
-剧本: メインシナリオ：嶋野まち（詠野万知子） サブシナリオ：合同会社Re,AER(影山千博、彩月レイ、呉西しの)、景山千博
-音乐: 市川淳
-人物设定: 煮たか
-原画: 煮たか
-otomate.jp/byakko/
+// BYAKKO ～四神部隊炎恋記～
+// 开发: オトメイト
+// 发行: アイディアファクトリー株式会社、オトメイト
+// 剧本: メインシナリオ：嶋野まち（詠野万知子） サブシナリオ：合同会社Re,AER(影山千博、彩月レイ、呉西しの)、景山千博
+// 音乐: 市川淳
+// 人物设定: 煮たか
+// 原画: 煮たか
+// otomate.jp/byakko/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -15,9 +15,9 @@ otomate.jp/byakko/
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
+    id: "game085", // 全局唯一ID，不可重复，如 game001
+    name: "BYAKKO ~四神部隊炎恋記~",
+    year: "2025",
     publisher: ["原厂发行商"],
     cnStudio: "汉化组/汉化厂商",
     writer: [
@@ -30,112 +30,90 @@ const gameData = {
         {name:"日文原画", lang:"ja"},
         {name:"英文原画", lang:"en"}
     ],
-    cover: "game/0.jpg", // 相对路径，游戏封面
+    cover: "game/085.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 佐野ゆずりは
         {
-            id: "g0_f01",
-            name: "女主A",
+            id: "g085_f01",
+            name: "佐野ゆずりは",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
                 { srcList: ["char/001/女主A.jpg",
                             "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 普通可攻略男主 male
+        // 秋月栄次郎
         {
-            id: "g0_m01",
-            name: "男主1",
+            id: "g085_m01",
+            name: "秋月栄次郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/001/男主1.jpg",
                             "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 飯山貞吉
         {
-            id: "g0_m02",
-            name: "男主2",
+            id: "g085_m02",
+            name: "飯山貞吉",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 石谷虎之助
         {
-            id: "g0_m03",
-            name: "男主3",
+            id: "g085_m03",
+            name: "石谷虎之助",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 神崎宗十郎
         {
-            id: "g0_m04",
-            name: "男主4",
+            id: "g085_m04",
+            name: "神崎宗十郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 岡本三郎
         {
-            id: "g0_m05",
-            name: "男主5",
+            id: "g085_m05",
+            name: "岡本三郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // 山瀬大蔵
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
+            id: "g085_m06",
+            name: "山瀬大蔵",
             gender: "male",
             isHidden: false,
-            isFD: true,
+            isFD: false,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/g0_m01_1.jpg",
+                            "char/g001_m0_2.jpg"], type: "base" },
             ]
         }
     ]
