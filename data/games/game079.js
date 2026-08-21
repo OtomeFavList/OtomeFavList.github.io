9dc4c27e7dc66494c3cfac701070524cc17670e5
OtomeFavList <otomefavlist@163.com>
2026-08-22 04:29:02 +0800

    Update game079.js

--- data/games/game079.js
+++ data/games/game079.js
@@ -35,10 +35,10 @@ const gameData = {
     ],
     cover: "game/0.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 桜庭アスカ
         {
             id: "g0_f01",
-            name: "女主A",
+            name: "桜庭アスカ",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
@@ -49,10 +49,10 @@ const gameData = {
                 { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 普通可攻略男主 male
+        // 帽子屋
         {
             id: "g0_m01",
-            name: "男主1",
+            name: "帽子屋",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -63,10 +63,10 @@ const gameData = {
                 { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // チェシャ猫
         {
             id: "g0_m02",
-            name: "男主2",
+            name: "チェシャ猫",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -77,10 +77,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // キング
         {
             id: "g0_m03",
-            name: "男主3",
+            name: "キング",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -91,10 +91,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 黒うさぎ
         {
             id: "g0_m04",
-            name: "男主4",
+            name: "黒うさぎ",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -105,10 +105,10 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 三月ウサギ
         {
             id: "g0_m05",
-            name: "男主5",
+            name: "三月ウサギ",
             gender: "male",
             isHidden: false,
             isFD: false,
@@ -119,26 +119,18 @@ const gameData = {
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // 裏ありす
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
+            id: "g0_m05",
+            name: "三月ウサギ",
             gender: "male",
             isHidden: false,
-            isFD: true,
+            isFD: false,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/g0_m01_1.jpg",
+                            "char/g001_m0_2.jpg"], type: "base" },
+                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
+                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         }
     ]
