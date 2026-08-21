9c969f6bd7498dd9ee6592e08babf38e2b895f70
OtomeFavList <otomefavlist@163.com>
2026-08-21 14:42:07 +0800

    Update game089.js

--- data/games/game089.js
+++ data/games/game089.js
@@ -1,16 +1,46 @@
-DIG-ROCK -Documentary of Youthful Sounds-
-开发: ALTERGEAR、デザインファクトリー
-发行: アイディアファクトリー
-剧本: 関涼子
-原作: MintLip
-平台: Nintendo Switch
-游戏类型: ドキュメンタリーADV
-发行日期: 2025-08-28
+BROTHERS CONFLICT Precious Baby
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+原画: ウダジョ
+别名: BROTHERS CONFLICT Precious Baby for Nintendo Switch
+ブラザース コンフリクト プレシャス ベイビー
+剧本: 小林悠奈、川澄あみ
+企画: 叶瀬あつこ
+原案: 叶瀬あつこ
+链接: ErogameScape VNDB
+官方网站: otomate.jp/bc/pb/
+
+BROTHERS CONFLICT  Passion Pink
+中文名: 兄弟战争 激情粉红
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+主题歌作曲: 小山哉枝、小野貴光、河合英嗣
+主题歌演出: 鈴村健一、鳥海浩輔
+原画: ウダジョ
+企画: 叶瀬あつこ
+主题歌作词: 小山哉枝
+官方网站: otomate.jp/bc/
+
+BROTHERS CONFLICT Brilliant Blue
+中文名: 兄弟战争 闪亮蔚蓝
+开发: オトメイト
+发行: アイディアファクトリー株式会社
+音乐: TrioDesign
+人物设定: ウダジョ
+主题歌作曲: L'aide、吉田和人
+主题歌演出: 前野智昭、鈴村健一、鳥海浩輔
+原画: ウダジョ
+平台: PSP
+游戏类型: ちょっとケンカなキョ―ダイ間恋愛SLG
+游玩人数: 1
+发行日期: 2013-09-12
 售价:
-通常版 8,580円（税込）
-特装版 10,780円（税込）
-ＤＬ版 8,580円（税込）
-导演: General Director: 久保嶋愛香（ティームエンタテインメント）、Game Director: 椎名あとり
-人物设定: 真久
-原画: 猫井アユ（Impish Crow）、六月ロク（RUBIA Leopard）
-官方网站: ideaf.co.jp/altergear/dig-rock/
+通常版 6,380 円（税込）
+限定版 8,580 円（税込）
+企画: 叶瀬あつこ
+主题歌作词: 小山哉枝
+官方网站: otomate.jp/bc/
