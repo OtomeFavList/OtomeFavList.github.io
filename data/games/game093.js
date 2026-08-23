a33171c605a2c7828f2b348f9bd09c6f546e30fc
OtomeFavList <otomefavlist@163.com>
2026-08-23 23:27:41 +0800

    Update game093.js

--- data/games/game093.js
+++ data/games/game093.js
@@ -28,11 +28,11 @@ SD原画: うた
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game093", // 全局唯一ID，不可重复，如 game001
+    name: "UN:LOGICAL",
+    year: "2025",
+    publisher: ["LicoBiTs"],
+    cnStudio: "暂无",
     writer: [
         {name:"中文编剧", lang:"zh"},
         {name:"日文编剧", lang:"ja"},
