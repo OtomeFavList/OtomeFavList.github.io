274e027888347dc0ef3f3b2b64dc7cbbcbdb90e1
OtomeFavList <otomefavlist@163.com>
2026-08-23 22:55:00 +0800

    Update game091.js

--- data/games/game091.js
+++ data/games/game091.js
@@ -1,37 +1,37 @@
-Blackish House ←sideZ -Retour-
-开发: honeybee black
-发行: アリスマティック、エディア
-人物设定: カズアキ
-平台: Nintendo Switch
-游戏类型: ダメ人間カウンセリング恋愛ADV
-游玩人数: 1
-发行日期: 2026-06-25
-售价:
-通常版 8,580円（税込)
-特装版 14,080円（税込）
-链接: blackishhouse-edia.com/sidez/
-官方网站: honeybee-cd.com/BlackishHouse/switch_sideZ/
+// Blackish House ←sideZ -Retour-
+// 开发: honeybee black
+// 发行: アリスマティック、エディア
+// 人物设定: カズアキ
+// 平台: Nintendo Switch
+// 游戏类型: ダメ人間カウンセリング恋愛ADV
+// 游玩人数: 1
+// 发行日期: 2026-06-25
+// 售价:
+// 通常版 8,580円（税込)
+// 特装版 14,080円（税込）
+// 链接: blackishhouse-edia.com/sidez/
+// 官方网站: honeybee-cd.com/BlackishHouse/switch_sideZ/
 
-Blackish House ←sideZ
-开发: honeybee black
-发行: honeybee black
-剧本: 中村幸代（Noa、Sera线）高木彩佳（悠翔线）坂本美香（那由多线）、もち大福、株式会社エッジワークス（浅生柚子、相川暁子、春名佳純、葉山いずみ、深瀬カエル、郡具もえ、浅野エミイ、日野光里、芝原三恵子、佐久間yaya）
-人物设定: カズアキ
-主题歌作曲: あるるかん
-主题歌作词: あるるかん
-主题歌演出: 蒼井翔太
-原画: カズアキ
-导演: 中村幸代（共通、公演部分）
-平台: PC
-游戏类型: ダメ人間カウンセリング恋愛ADV
-游玩人数: 1
-发行日期: 2017-04-28
-售价:
-通常版 5,800 円＋税
-初回限定版 7,800 円＋税
-SDキャラクター: 三月リヒト
-链接: ErogameScape VNDB
-官方网站: honeybee-cd.com/BlackishHouse/sideZ/
+// Blackish House ←sideZ
+// 开发: honeybee black
+// 发行: honeybee black
+// 剧本: 中村幸代（Noa、Sera线）高木彩佳（悠翔线）坂本美香（那由多线）、もち大福、株式会社エッジワークス（浅生柚子、相川暁子、春名佳純、葉山いずみ、深瀬カエル、郡具もえ、浅野エミイ、日野光里、芝原三恵子、佐久間yaya）
+// 人物设定: カズアキ
+// 主题歌作曲: あるるかん
+// 主题歌作词: あるるかん
+// 主题歌演出: 蒼井翔太
+// 原画: カズアキ
+// 导演: 中村幸代（共通、公演部分）
+// 平台: PC
+// 游戏类型: ダメ人間カウンセリング恋愛ADV
+// 游玩人数: 1
+// 发行日期: 2017-04-28
+// 售价:
+// 通常版 5,800 円＋税
+// 初回限定版 7,800 円＋税
+// SDキャラクター: 三月リヒト
+// 链接: ErogameScape VNDB
+// 官方网站: honeybee-cd.com/BlackishHouse/sideZ/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -41,129 +41,87 @@ SDキャラクター: 三月リヒト
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game091", // 全局唯一ID，不可重复，如 game001
+    name: "Blackish House ←sideZ -Retour-",
+    year: "2026",
+    publisher: ["honeybee black"],
+    cnStudio: "暂无",
     writer: [
-        {name:"中文编剧", lang:"zh"},
-        {name:"日文编剧", lang:"ja"},
-        {name:"英文编剧", lang:"en"}
+        {name:"中村幸代", lang:"zh"},
+        {name:"高木彩佳", lang:"zh"},
+        {name:"坂本美香", lang:"zh"},
+        {name:"浅生柚子", lang:"zh"},
+        {name:"相川暁子", lang:"zh"},
+        {name:"春名佳純", lang:"zh"},
+        {name:"葉山いずみ", lang:"zh"},
+        {name:"深瀬カエル", lang:"zh"},
+        {name:"郡具もえ", lang:"zh"},
+        {name:"浅野エミイ", lang:"zh"},
+        {name:"日野光里", lang:"zh"},
+        {name:"芝原三恵子", lang:"zh"},
+        {name:"佐久間yaya", lang:"zh"},
+        {name:"もち大福", lang:"ja"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"カズアキ", lang:"ja"}
     ],
-    cover: "game/0.jpg", // 相对路径，游戏封面
+    cover: "game/091.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 亜麻乃雛
         {
-            id: "g0_f01",
-            name: "女主A",
+            id: "g091_f01",
+            name: "亜麻乃雛",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["char/001/女主A.jpg",
-                            "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+                { srcList: ["char/001/女主A.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 阿久根セラ
         {
-            id: "g0_m01",
-            name: "男主1",
+            id: "g091_m01",
+            name: "阿久根セラ",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/001/男主1.jpg",
-                            "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
+                { srcList: ["char/001/男主1.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 有村乃亜
         {
-            id: "g0_m02",
-            name: "男主2",
+            id: "g091_m02",
+            name: "有村乃亜",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // 普通可攻略男主 male
+        // 桜坂悠翔
         {
-            id: "g0_m03",
-            name: "男主3",
+            id: "g091_m03",
+            name: "桜坂悠翔",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // 普通可攻略男主 male
+        // 結城那由多
         {
-            id: "g0_m04",
-            name: "男主4",
+            id: "g091_m04",
+            name: "結城那由多",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // 普通可攻略男主 male
-        {
-            id: "g0_m05",
-            name: "男主5",
-            gender: "male",
-            isHidden: false,
-            isFD: false,
-            images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
-        {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
-            ]
-        }
     ]
 };
 
