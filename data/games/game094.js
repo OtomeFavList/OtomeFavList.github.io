a35d458dc0156d53c53b00a01e010daa06e69dea
OtomeFavList <otomefavlist@163.com>
2026-08-23 23:46:33 +0800

    Update game094.js

--- data/games/game094.js
+++ data/games/game094.js
@@ -1,49 +1,50 @@
 BLACK WOLVES SAGA -Weiβ und Schwarz- for Nintendo Switch
+https://www.otomate.jp/bws/switch/
 
+// BLACK WOLVES SAGA -Weiβ und Schwarz-
+// 开发: Rejet、オトメイト
+// 发行: Rejet、アイディアファクトリー株式会社、オトメイト
+// 主题歌演出: lasah
+// 原画: 黒裄
+// 原作: Rejet
+// 别名: BLACK WOLVES SAGA -Weiβ und Schwarz- for Nintendo Switch
+// 平台:
+// Nintendo Switch PSV
+// 官方网站: otomate.jp/bws/wus/
+  
+// BLACK WOLVES SAGA -Bloody Nightmare-
+// 中文名: 黑狼传说 血色梦魇
+// 开发: Rejet
+// 发行: Rejet
+// 音乐: 光田康典、土屋俊輔、亀岡夏海
+// 人物设定: 黒裄
+// 主题歌作曲: 光田康典、亀岡夏海
+// 主题歌作词: 岩崎大介
+// 主题歌演出: lasah
+// 插入歌演出: lasah、梶裕貴
+// 原画: 黒裄
+// 程序: 細谷博子
+// 剧本: 山田かのこ
+// 插入歌作曲: 土屋俊輔
+// 插入歌作词: 土屋俊輔
+// 导演: 伊東紗希
+// 链接: ErogameScape VNDB
+// 官方网站: rejetweb.jp/bws/top/
+  
+// BLACK WOLVES SAGA -Last Hope-
+// 开发: オトメイト、Rejet
+// 发行: オトメイト、アイディアファクトリー株式会社
+// 剧本: 山田かのこ、SOM、砂原有季
+// 音乐: 土屋俊輔、光田康典
+// 人物设定: 黒裄
+// 主题歌作曲: 土屋俊輔、亀岡夏海
+// 主题歌作词: lasah、岩崎大介、亀岡夏海
+// 主题歌演出: 梶裕貴、lasah
+// 插入歌演出: lasah
+// 原画: 黒裄
+// 导演: 伊東紗希
+// 官方网站: rejetweb.jp/bws
 
-BLACK WOLVES SAGA -Weiβ und Schwarz-
-开发: Rejet、オトメイト
-发行: Rejet、アイディアファクトリー株式会社、オトメイト
-主题歌演出: lasah
-原画: 黒裄
-原作: Rejet
-别名: BLACK WOLVES SAGA -Weiβ und Schwarz- for Nintendo Switch
-平台:
-Nintendo Switch PSV
-官方网站: otomate.jp/bws/wus/
-  
-BLACK WOLVES SAGA -Bloody Nightmare-
-中文名: 黑狼传说 血色梦魇
-开发: Rejet
-发行: Rejet
-音乐: 光田康典、土屋俊輔、亀岡夏海
-人物设定: 黒裄
-主题歌作曲: 光田康典、亀岡夏海
-主题歌作词: 岩崎大介
-主题歌演出: lasah
-插入歌演出: lasah、梶裕貴
-原画: 黒裄
-程序: 細谷博子
-剧本: 山田かのこ
-插入歌作曲: 土屋俊輔
-插入歌作词: 土屋俊輔
-导演: 伊東紗希
-链接: ErogameScape VNDB
-官方网站: rejetweb.jp/bws/top/
-  
-BLACK WOLVES SAGA -Last Hope-
-开发: オトメイト、Rejet
-发行: オトメイト、アイディアファクトリー株式会社
-剧本: 山田かのこ、SOM、砂原有季
-音乐: 土屋俊輔、光田康典
-人物设定: 黒裄
-主题歌作曲: 土屋俊輔、亀岡夏海
-主题歌作词: lasah、岩崎大介、亀岡夏海
-主题歌演出: 梶裕貴、lasah
-插入歌演出: lasah
-原画: 黒裄
-导演: 伊東紗希
-官方网站: rejetweb.jp/bw
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
 // 图片路径采用相对路径格式（game/xxx.jpg 或 char/xxx/xxx.jpg）
@@ -52,7 +53,7 @@ BLACK WOLVES SAGA -Last Hope-
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
+    id: "game094", // 全局唯一ID，不可重复，如 game001
     name: "示例乙女游戏",
     year: "20",
     publisher: ["原厂发行商"],
