0a00fa632dab05982e7db03de202d3ac0d80f1de
OtomeFavList <otomefavlist@163.com>
2026-08-23 22:45:04 +0800

    Update game090.js

--- data/games/game090.js
+++ data/games/game090.js
@@ -1,29 +1,29 @@
-Blackish House sideA→ -Retour-
-开发: honeybee black
-发行: アリスマティック、エディア
-人物设定: カズアキ
-平台: Nintendo Switch
-官方网站: honeybee-cd.com/BlackishHouse/switch_sideA/
+// Blackish House sideA→ -Retour-
+// 开发: honeybee black
+// 发行: アリスマティック、エディア
+// 人物设定: カズアキ
+// 平台: Nintendo Switch
+// 官方网站: honeybee-cd.com/BlackishHouse/switch_sideA/
 
-Blackish House sideA→
-开发: honeybee black
-发行: honeybee black
-剧本: 中村幸代（円线）、みなづきともこ、ゆずみやともめ、乙月ありさ、伊原恵、桜ゆづき、柿本悠理、煌矢まな、谷村知子
-人物设定: カズアキ
-主题歌作曲: あるるかん
-主题歌作词: あるるかん
-主题歌演出: 石川界人
-插入歌演出: 蒼井翔太
-原画: カズアキ
-导演: 高木彩佳（共通、公演部分、其它3线）
-平台: PC
-游戏类型: ダメ人間カウンセリング恋愛ADV
-游玩人数: 1
-发行日期: 2016-08-26
-售价:
-通常版 6,264 円
-初回限定版 8,424 円
-官方网站: honeybee-cd.com/BlackishHouse/sideA/
+// Blackish House sideA→
+// 开发: honeybee black
+// 发行: honeybee black
+// 剧本: 中村幸代（円线）、みなづきともこ、ゆずみやともめ、乙月ありさ、伊原恵、桜ゆづき、柿本悠理、煌矢まな、谷村知子
+// 人物设定: カズアキ
+// 主题歌作曲: あるるかん
+// 主题歌作词: あるるかん
+// 主题歌演出: 石川界人
+// 插入歌演出: 蒼井翔太
+// 原画: カズアキ
+// 导演: 高木彩佳（共通、公演部分、其它3线）
+// 平台: PC
+// 游戏类型: ダメ人間カウンセリング恋愛ADV
+// 游玩人数: 1
+// 发行日期: 2016-08-26
+// 售价:
+// 通常版 6,264 円
+// 初回限定版 8,424 円
+// 官方网站: honeybee-cd.com/BlackishHouse/sideA/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -37,7 +37,7 @@ const gameData = {
     name: "Blackish House sideA→ -Retour-",
     year: "2025",
     publisher: ["honeybee black"],
-    cnStudio: "汉化组/汉化厂商",
+    cnStudio: "暂无",
     writer: [
         {name:"中村幸代", lang:"zh"},
         {name:"乙月ありさ", lang:"zh"},
@@ -54,110 +54,60 @@ const gameData = {
     ],
     cover: "game/090.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 亜麻乃雛
         {
             id: "g090_f01",
-            name: "女主A",
+            name: "亜麻乃雛",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
-                { srcList: ["char/001/女主A.jpg",
-                            "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
+                { srcList: ["char/001/女主A.jpg"], type: "base" },      // 默认基础图，始终加载
             ]
         },
-        // 普通可攻略男主 male
+        // 姫崎藤吾
         {
-            id: "g0_m01",
-            name: "男主1",
+            id: "g090_m01",
+            name: "姫崎藤吾",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/001/男主1.jpg",
                             "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // 久世円
         {
-            id: "g0_m02",
-            name: "男主2",
+            id: "g090_m02",
+            name: "久世円",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // 普通可攻略男主 male
+        // 宇賀神澪
         {
-            id: "g0_m03",
-            name: "男主3",
+            id: "g090_m03",
+            name: "宇賀神澪",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         },
-        // 普通可攻略男主 male
+        // 椎葉剛
         {
-            id: "g0_m04",
-            name: "男主4",
+            id: "g090_m04",
+            name: "椎葉剛",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 普通可攻略男主 male
-        {
-            id: "g0_m05",
-            name: "男主5",
-            gender: "male",
-            isHidden: false,
-            isFD: false,
-            images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
-        {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/g0_m01_1.jpg"], type: "base" },
             ]
         }
     ]
