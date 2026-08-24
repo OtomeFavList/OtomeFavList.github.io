f6582d84c6619c070bad6d1b3f2ebe9498bd97cb
OtomeFavList <otomefavlist@163.com>
2026-08-24 22:52:13 +0800

    Update game099.js

--- data/games/game099.js
+++ data/games/game099.js
@@ -1,18 +1,18 @@
-CRAZY CHA!N -エルピスの鎖-
-开发: オトメイト
-发行: アイディアファクトリー
-剧本: 中山智美
-原画: 読
-导演: 吉田ミサ
-平台: Nintendo Switch
-游戏类型: 女性向け恋愛ADV
-游玩人数: 1人
-发行日期: 2026-07-09
-售价:
-通常版、DL版 8,580円(税込)
-特装版 10,780円(税込)
-オトメイト スイート BOX 19,800円(税込)
-官方网站: otomate.jp/crazy_chain/
+// CRAZY CHA!N -エルピスの鎖-
+// 开发: オトメイト
+// 发行: アイディアファクトリー
+// 剧本: 中山智美
+// 原画: 読
+// 导演: 吉田ミサ
+// 平台: Nintendo Switch
+// 游戏类型: 女性向け恋愛ADV
+// 游玩人数: 1人
+// 发行日期: 2026-07-09
+// 售价:
+// 通常版、DL版 8,580円(税込)
+// 特装版 10,780円(税込)
+// オトメイト スイート BOX 19,800円(税込)
+// 官方网站: otomate.jp/crazy_chain/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -22,127 +22,101 @@ CRAZY CHA!N -エルピスの鎖-
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game0", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game099", // 全局唯一ID，不可重复，如 game001
+    name: "CRAZY CHA!N -エルピスの鎖-",
+    year: "2026",
+    publisher: ["Otomate"],
+    cnStudio: "暂无",
     writer: [
-        {name:"中文编剧", lang:"zh"},
-        {name:"日文编剧", lang:"ja"},
-        {name:"英文编剧", lang:"en"}
+        {name:"中山智美", lang:"zh"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"読", lang:"zh"}
     ],
-    cover: "game/0.jpg", // 相对路径，游戏封面
+    cover: "game/099.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // ソフィア
         {
-            id: "g0_f01",
-            name: "女主A",
+            id: "g099_f01",
+            name: "ソフィア",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
             images: [
                 { srcList: ["char/001/女主A.jpg",
                             "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
             ]
         },
-        // 普通可攻略男主 male
+        // クロノール
         {
-            id: "g0_m01",
-            name: "男主1",
+            id: "g099_m01",
+            name: "クロノール",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/001/男主1.jpg",
                             "char/001/男主12.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/男主13.jpg"], type: "hidden" },
-                { srcList: ["char/001/男主14.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // フラトニス·ライラプス
         {
-            id: "g0_m02",
-            name: "男主2",
+            id: "g099_m02",
+            name: "フラトニス·ライラプス",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // God
         {
-            id: "g0_m03",
-            name: "男主3",
+            id: "g099_m03",
+            name: "God",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // リーズ·ルネ·シャナ
         {
-            id: "g0_m04",
-            name: "男主4",
+            id: "g099_m04",
+            name: "リーズ·ルネ·シャナ",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 普通可攻略男主 male
+        // レクス·ド·クラーシェル
         {
-            id: "g0_m05",
-            name: "男主5",
+            id: "g099_m05",
+            name: "レクス·ド·クラーシェル",
             gender: "male",
             isHidden: false,
             isFD: false,
             images: [
                 { srcList: ["char/g0_m01_1.jpg",
                             "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
             ]
         },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
+        // ヴァーデス
         {
-            id: "g0_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g0_fd01",
-            name: "续作FD新增角色",
+            id: "g099_m06",
+            name: "ヴァーデス",
             gender: "male",
             isHidden: false,
-            isFD: true,
+            isFD: false,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/g0_m01_1.jpg",
+                            "char/g001_m0_2.jpg"], type: "base" },
             ]
         }
     ]
