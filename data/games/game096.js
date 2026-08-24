64da3f1abb4a29d6159156fe00c62c810af2e44f
OtomeFavList <otomefavlist@163.com>
2026-08-24 20:14:24 +0800

    Update game096.js

--- data/games/game096.js
+++ data/games/game096.js
@@ -1,28 +1,28 @@
-闇色の魔珠
-中文名: 暗色的魔珠
-开发: Daisy²
-发行: Daisy²、株式会社プロトタイプ
-剧本: 雨宮うた
-音乐: 細井聡司(細井そうし)
-人物设定: めろ
-主题歌演出: SUZUKA、YURIA、中原涼
-原画: めろ
-平台:
-PC Nintendo Switch
-游戏类型: 全年齡乙女ゲーム
-游玩人数: 1
-发行日期:
-PC版 2023-12-22
-Nintendo Switch版 2025-11-20
-售价:
-PC豪華特装版 11,000円（税込）
-PC通常版 8,250円（税込）
-Nintendo Switchパッケージ通常版 6,820 円（税込）
-Nintendo Switchパッケージ初回限定版 11,880 円（税込）
-Nintendo Switchダウンロード版 5,800 円（税込）
-主题歌作曲: 細井そうし
-链接: ErogameScape VNDB
-官方网站: daisy-daisy.in/products/majyu/
+// 闇色の魔珠
+// 中文名: 暗色的魔珠
+// 开发: Daisy²
+// 发行: Daisy²、株式会社プロトタイプ
+// 剧本: 雨宮うた
+// 音乐: 細井聡司(細井そうし)
+// 人物设定: めろ
+// 主题歌演出: SUZUKA、YURIA、中原涼
+// 原画: めろ
+// 平台:
+// PC Nintendo Switch
+// 游戏类型: 全年齡乙女ゲーム
+// 游玩人数: 1
+// 发行日期:
+// PC版 2023-12-22
+// Nintendo Switch版 2025-11-20
+// 售价:
+// PC豪華特装版 11,000円（税込）
+// PC通常版 8,250円（税込）
+// Nintendo Switchパッケージ通常版 6,820 円（税込）
+// Nintendo Switchパッケージ初回限定版 11,880 円（税込）
+// Nintendo Switchダウンロード版 5,800 円（税込）
+// 主题歌作曲: 細井そうし
+// 链接: ErogameScape VNDB
+// 官方网站: daisy-daisy.in/products/majyu/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -45,10 +45,10 @@ const gameData = {
     ],
     cover: "game/096.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // ガリカ·ローサ·スターニス
         {
             id: "g096_f01",
-            name: "女主A",
+            name: "ガリカ·ローサ·スターニス",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,        // true=续作/FD专属角色，FD开关控制是否展示该角色
