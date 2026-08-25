01a7a4ee49a6ffac85f333340e2cdf29c49842e3
OtomeFavList <otomefavlist@163.com>
2026-08-25 21:26:02 +0800

    Update game103.js

--- data/games/game103.js
+++ data/games/game103.js
@@ -1,53 +1,51 @@
-月影の鎖 -錯乱パラノイア-
-开发: TAKUYO
-发行: TAKUYO
-人物设定: ヒロセアヅミ
-主题歌作曲: Morrigan、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
-主题歌作词: 片霧烈火、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
-主题歌演出: 片霧烈火、ラムソアーズ（Nintendo Switch版）
-原画: ヒロセアヅミ
-剧本: 白鳥ユアン、井上愁、其他
-音乐: 森藤晶司
-链接: ErogameScape VNDB
-官方网站: takuyo.co.jp/products/getsueifd/getsueifdvita/index.html
+// 月影の鎖 -錯乱パラノイア-
+// 开发: TAKUYO
+// 发行: TAKUYO
+// 人物设定: ヒロセアヅミ
+// 主题歌作曲: Morrigan、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
+// 主题歌作词: 片霧烈火、ざっとん（Nintendo Switch版）、カケリネ（Nintendo Switch版）
+// 主题歌演出: 片霧烈火、ラムソアーズ（Nintendo Switch版）
+// 原画: ヒロセアヅミ
+// 剧本: 白鳥ユアン、井上愁、其他
+// 音乐: 森藤晶司
+// 链接: ErogameScape VNDB
+// 官方网站: takuyo.co.jp/products/getsueifd/getsueifdvita/index.html
 
-中文名: 月影之锁 错乱偏执狂
-开发: TAKUYO
-发行: TAKUYO
-剧本: 白鳥ユアン（共通、大井川、榛名线，猪口线部分）／藤元（藤文）（神乐坂线、望月神乐坂共通）／井上愁（望月线，猪口线部分）／犬小屋煙突
-音乐: 森藤晶司
-人物设定: ヒロセアヅミ
-主题歌作曲: Morrigan
-主题歌作词: 片霧烈火
-主题歌演出: 片霧烈火
-原画: ヒロセアヅミ
-官方网站: takuyo.co.jp/products/getsuei/index.html
+// 中文名: 月影之锁 错乱偏执狂
+// 开发: TAKUYO
+// 发行: TAKUYO
+// 剧本: 白鳥ユアン（共通、大井川、榛名线，猪口线部分）／藤元（藤文）（神乐坂线、望月神乐坂共通）／井上愁（望月线，猪口线部分）／犬小屋煙突
+// 音乐: 森藤晶司
+// 人物设定: ヒロセアヅミ
+// 主题歌作曲: Morrigan
+// 主题歌作词: 片霧烈火
+// 主题歌演出: 片霧烈火
+// 原画: ヒロセアヅミ
+// 官方网站: takuyo.co.jp/products/getsuei/index.html
 
-月影の鎖 -狂爛モラトリアム-
-开发: TAKUYO
-发行: TAKUYO
-剧本: 白鳥ユアン／井上愁／犬小屋煙突／関口琴子／そよかぜ御舟
-音乐: 森藤晶司
-人物设定: ヒロセアヅミ
-主题歌作曲: 柊奈緒
-主题歌作词: 柊奈緒
-主题歌演出: 柊奈緒、片霧烈火
-原画: ヒロセアヅミ
-官方网站: takuyo.co.jp/products/getsueifd/index.html
+// 月影の鎖 -狂爛モラトリアム-
+// 开发: TAKUYO
+// 发行: TAKUYO
+// 剧本: 白鳥ユアン／井上愁／犬小屋煙突／関口琴子／そよかぜ御舟
+// 音乐: 森藤晶司
+// 人物设定: ヒロセアヅミ
+// 主题歌作曲: 柊奈緒
+// 主题歌作词: 柊奈緒
+// 主题歌演出: 柊奈緒、片霧烈火
+// 原画: ヒロセアヅミ
+// 官方网站: takuyo.co.jp/products/getsueifd/index.html
 
-开发: TAKUYO
-发行: TAKUYO
-人物设定: ヒロセアヅミ
-主题歌作曲: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、Morrigan
-主题歌作词: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、片霧烈火
-主题歌演出: 柊奈緒、片霧烈火、ラムソアーズ（Nintendo Switch版）
-原画: ヒロセ アヅミ
-剧本: 白鳥ユアン、井上愁、其他
-音乐: 森藤晶司
-链接: ErogameScape VNDB
-官方网站: takuyo.co.jp/products/getsueifd/getsueifdvita/index.html
-
-
+// 开发: TAKUYO
+// 发行: TAKUYO
+// 人物设定: ヒロセアヅミ
+// 主题歌作曲: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、Morrigan
+// 主题歌作词: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、片霧烈火
+// 主题歌演出: 柊奈緒、片霧烈火、ラムソアーズ（Nintendo Switch版）
+// 原画: ヒロセ アヅミ
+// 剧本: 白鳥ユアン、井上愁、其他
+// 音乐: 森藤晶司
+// 链接: ErogameScape VNDB
+// 官方网站: takuyo.co.jp/products/getsueifd/getsueifdvita/index.html
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
