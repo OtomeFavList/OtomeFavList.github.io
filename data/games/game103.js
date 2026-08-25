c5b77e09275b381f8becd8c9685476ae548a93aa
OtomeFavList <otomefavlist@163.com>
2026-08-25 20:56:01 +0800

    Update game103.js with new game details

--- data/games/game103.js
+++ data/games/game103.js
@@ -41,7 +41,7 @@
 主题歌作曲: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、Morrigan
 主题歌作词: 柊奈緒、ざっとん（Nintendo Switch版）、KAKERU（Nintendo Switch版）、片霧烈火
 主题歌演出: 柊奈緒、片霧烈火、ラムソアーズ（Nintendo Switch版）
-原画: ヒロセ アヅミ、ヒロセアヅミ
+原画: ヒロセ アヅミ
 剧本: 白鳥ユアン、井上愁、其他
 音乐: 森藤晶司
 链接: ErogameScape VNDB
@@ -57,27 +57,26 @@
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game1", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game103", // 全局唯一ID，不可重复，如 game001
+    name: "月影の鎖 -錯乱パラノイア-",
+    year: "2023",
+    publisher: ["TAKUYO"],
+    cnStudio: "暂无",
     writer: [
-        {name:"中文编剧", lang:"zh"},
-        {name:"日文编剧", lang:"ja"},
-        {name:"英文编剧", lang:"en"}
+        {name:"白鳥ユアン", lang:"zh"},
+        {name:"藤元", lang:"zh"},
+        {name:"井上愁", lang:"zh"},
+        {name:"犬小屋煙突", lang:"zh"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"ヒロセ アヅミ", lang:"ja"}
     ],
-    cover: "game/1.jpg", // 相对路径，游戏封面
+    cover: "game/103.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
+        // 冬浦めぐみ
         {
             id: "g1_f01",
-            name: "女主A",
+            name: "冬浦めぐみ",
             gender: "female",
             isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
             isFD: false,    // true=续作/FD专属角色，开关控制是否展示该角色
@@ -147,57 +146,6 @@ const gameData = {
                             "char/g001_m0_2.jpg"], type: "base" },
                 { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
                 { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 普通可攻略男主 male
-        {
-            id: "g1_m05",
-            name: "男主5",
-            gender: "male",
-            isHidden: false,
-            isFD: false,
-            isSub: true,
-            images: [
-                { srcList: ["char/g0_m01_1.jpg",
-                            "char/g001_m0_2.jpg"], type: "base" },
-                { srcList: ["char/g0_m01_3.jpg"], type: "hidden" },
-                { srcList: ["char/g0_m01_4.jpg"], type: "fd" }
-            ]
-        },
-        // 隐藏角色（isHidden=true → 开关开启才显示整个角色卡片）
-        {
-            id: "g1_h01",
-            name: "隐藏攻略角色",
-            gender: "male",
-            isHidden: true,
-            isFD: false,
-            isSub: true,
-            images: [
-                { srcList: ["char/g001_h01_1.jpg"], type: "base" }
-            ]
-        },
-        // FD续作限定角色（isFD=true → FD开关开启才显示整个角色卡片）
-        {
-            id: "g1_fd01",
-            name: "续作FD新增角色",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            isSub: true,
-            images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
-            ]
-        },
-        // 配角角色（isSub=true → 开关开启才显示整个角色卡片）
-        {
-            id: "g1_fd01",
-            name: "配角",
-            gender: "male",
-            isHidden: false,
-            isFD: false,
-            isSub: true,
-            images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
             ]
         }
     ]
