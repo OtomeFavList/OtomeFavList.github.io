ab0e5bef48e8454f2fc7b1d27de1b3ef11c4b5a5
OtomeFavList <otomefavlist@163.com>
2026-08-26 03:10:16 +0800

    Update game107.js

--- data/games/game107.js
+++ data/games/game107.js
@@ -1,28 +1,28 @@
-ときめきメモリアル Girl's Side 3rd Story
-中文名: 心跳回忆女生版3
-开发: KONAMI
-发行: KONAMI
-剧本: 内田明理、田島あきこ、ちゃい、小松原里枝子、神山敬介、松岡功、川名良昌、株式会社エム・ツー、有限会社codeX、コーデックス
-人物设定: 小松原里枝子
-主题歌作曲: Momo、近藤敦教
-企画: 内田明理、谷口淳一、小松原里枝子
-制作人: 内田明理
-别名: GS3
-ときめきメモリアル Girl's Side 3rd Story for Nintendo Switch
-平台:
-NDS Nintendo Switch
-游戏类型: 恋愛シミュレーションゲーム
-游玩人数: 1
-发行日期:
-NDS版 2010-06-24
-Nintendo Switch版 2024-02-14
-售价:
-NDS版 5,500 円（税込）
-Nintendo Switch版 5,995 円（税込）
-主题歌作词: 平井森太郎
-主题歌演出: Shigeru、toshi
-链接: ErogameScape VNDB
-官方网站: konami.com/game/Girls_Side/3rd_Story/
+// ときめきメモリアル Girl's Side 3rd Story
+// 中文名: 心跳回忆女生版3
+// 开发: KONAMI
+// 发行: KONAMI
+// 剧本: 内田明理、田島あきこ、ちゃい、小松原里枝子、神山敬介、松岡功、川名良昌、株式会社エム・ツー、有限会社codeX、コーデックス
+// 人物设定: 小松原里枝子
+// 主题歌作曲: Momo、近藤敦教
+// 企画: 内田明理、谷口淳一、小松原里枝子
+// 制作人: 内田明理
+// 别名: GS3
+// ときめきメモリアル Girl's Side 3rd Story for Nintendo Switch
+// // 平台:
+// NDS Nintendo Switch
+// 游戏类型: 恋愛シミュレーションゲーム
+// 游玩人数: 1
+// 发行日期:
+// NDS版 2010-06-24
+// Nintendo Switch版 2024-02-14
+// 售价:
+// NDS版 5,500 円（税込）
+// Nintendo Switch版 5,995 円（税込）
+// 主题歌作词: 平井森太郎
+// 主题歌演出: Shigeru、toshi
+// 链接: ErogameScape VNDB
+// 官方网站: konami.com/game/Girls_Side/3rd_Story/
 
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
@@ -160,9 +160,6 @@ const gameData = {
         }
     ]
 };
-
-// ✅新版导出！不要使用window.gameDataList.push！
-export { gameData };
 
 // ✅新版导出！不要使用window.gameDataList.push！
 export { gameData };
