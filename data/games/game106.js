5a3364d39138e67c58b82fbd0836cf77b6e981b4
OtomeFavList <otomefavlist@163.com>
2026-08-25 23:36:45 +0800

    Update game106.js

--- data/games/game106.js
+++ data/games/game106.js
@@ -38,38 +38,21 @@ Nintendo Switch版 5,995 円（税込）
 // 仅需要在 main.js 顶部 🚨gameIdList数组追加编号
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
-    id: "game1", // 全局唯一ID，不可重复，如 game001
-    name: "示例乙女游戏",
-    year: "20",
-    publisher: ["原厂发行商"],
-    cnStudio: "汉化组/汉化厂商",
+    id: "game106", // 全局唯一ID，不可重复，如 game001
+    name: "ときめきメモリアル Girl’s Side 2nd Season",
+    year: "2024",
+    publisher: ["KONAMI"],
+    cnStudio: "暂无",
     writer: [
         {name:"中文编剧", lang:"zh"},
         {name:"日文编剧", lang:"ja"},
         {name:"英文编剧", lang:"en"}
     ],
     art: [
-        {name:"中文原画", lang:"zh"},
-        {name:"日文原画", lang:"ja"},
-        {name:"英文原画", lang:"en"}
+        {name:"小松原里枝子", lang:"zh"}
     ],
     cover: "game/1.jpg", // 相对路径，游戏封面
     charList: [
-        // 女主模板 female
-        {
-            id: "g1_f01",
-            name: "女主A",
-            gender: "female",
-            isHidden: false,    // true=隐藏角色，开关控制是否展示该角色
-            isFD: false,    // true=续作/FD专属角色，开关控制是否展示该角色
-            isSub: true,    // true=配角，开关控制是否展示该角色
-            images: [
-                { srcList: ["char/001/女主A.jpg",
-                            "char/001/女主A2.jpg"], type: "base" },      // 默认基础图，始终加载
-                { srcList: ["char/001/女主A3.jpg"], type: "hidden" },     // 需要开启隐藏角色开关才展示的图片
-                { srcList: ["char/001/女主A4.jpg"], type: "fd" }          // 需要开启FD开关才展示的图片
-            ]
-        },
         // 普通可攻略男主 male
         {
             id: "g1_m01",
