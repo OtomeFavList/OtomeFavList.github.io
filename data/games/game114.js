78d2be3a254b80ea55e77f82663f312f77eabc6f
OtomeFavList <otomefavlist@163.com>
2026-08-27 13:01:22 +0800

    Create game114.js

---
+++ data/games/game114.js
@@ -0,0 +1,20 @@
+OZMAFIA!!-vivace-
+开发: Poni-Pachet
+发行: dramatic create（ヒューネックス株式会社）
+剧本: ゆーます
+人物设定: さとい
+原画: さとい
+原作: Poni-Pachet SY
+别名: オズマフィア!!-ヴィヴァーチェ-
+官方网站: dramaticcreate.com/ozmafia/
+
+OZMAFIA!!
+开发: Poni-Pachet
+发行: Poni-Pachet
+剧本: ゆーます
+音乐: project lights
+人物设定: さとい
+插入歌演出: Lira
+原画: さとい
+别名: オズマフィア
+官方网站: oz.ponipa.com/index.html
