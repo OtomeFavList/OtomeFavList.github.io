63b7d814bd007e83e8c41c55e2c3da417f2ead83
OtomeFavList <otomefavlist@163.com>
2026-08-27 18:29:48 +0800

    Create game130.js

---
+++ data/games/game130.js
@@ -0,0 +1,24 @@
+DYNAMIC CHORD feat.apple-polisher Remaster Edition
+https://dramaticcreate.com/dynamic/ns_app/?utm_source=chatgpt.com
+
+DYNAMIC CHORD feat.apple-polisher
+开发: honeybee black
+发行: honeybee black
+剧本: 中村幸代（なかむらさちよ）、新井菜津美（Plot）；浅生柚子、柚子みかん（なかむらさちよ）、絹夏（执笔）
+人物设定: 冨士原良
+主题歌演出: 蒼井翔太
+插入歌演出: 蒼井翔太
+原画: 冨士原良
+官方网站: honeybee-cd.com/dynamic/app/index.html
+
+DYNAMIC CHORD feat.apple-polisher V
+开发: honeybee black
+发行: dramatic create
+人物设定: 冨士原良
+主题歌演出: 蒼井翔太
+插入歌演出: 蒼井翔太
+原画: 冨士原良
+剧本: 浅生柚子
+官方网站: honeybee-cd.com/dynamic/vita-app/
+
+https://www.honeybee-cd.com/dynamic/app/?utm_source=chatgpt.com
