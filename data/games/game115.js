b972da65473c5a1110c85805846a44396c43ac64
OtomeFavList <otomefavlist@163.com>
2026-08-27 13:09:47 +0800

    Create game115.js

---
+++ data/games/game115.js
@@ -0,0 +1,31 @@
+逆転吉原
+中文名: 逆转吉原
+发行: Dogenzaka Lab
+人物设定: Hs
+原画: Hs
+别名: The Men of Yoshiwara
+开发: スマホカレシ
+官方网站: d3p.co.jp/sumakare/gyakuten/
+
+男遊郭
+开发: ひつじぐも、株式会社ブリッジ、ディースリー・パブリッシャー
+剧本: かほく麻緒、島田類、ひつじぐも
+音乐: 浅乃一
+人物设定: Hs
+主题歌作曲: AZ-MIX、タナカノブマサ、ロア健治、田中智之、浅乃一、野見山美貴、麿
+主题歌作词: yumico、サカモトヨシキ、タナカノブマサ、ロア健治、麿
+主题歌演出: 久野藍、柿原徹也、森久保祥太郎、置鮎龍太郎、野島健児、鳥海浩輔、黒田崇矢
+原画: Hs
+平台: PSV
+官方网站: d3p.co.jp/otokoyukaku/vita/
+  
+逆転吉原～扇屋編～
+中文名: 逆转吉原～扇屋编～
+开发: Dogenzaka Lab
+发行: Dogenzaka Lab、ディースリー・パブリッシャー
+人物设定: Hs
+原画: Hs
+别名: The Men of Yoshiwara: Ohgiya
+艶恋絵巻
+The Rosy Script
+官方网站: d3p.co.jp/ohgiya/
