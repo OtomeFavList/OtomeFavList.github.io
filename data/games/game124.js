591fdbeb12de867d05960d980c2b803d1a8f6c36
OtomeFavList <otomefavlist@163.com>
2026-08-27 17:11:38 +0800

    Create game124.js

---
+++ data/games/game124.js
@@ -0,0 +1,37 @@
+ゆのはなSpRING！ ～Mellow Times～ for Nintendo Switch
+中文名: 温泉之花！ ～Mellow Times～ for Nintendo Switch
+开发: オトメイト、デザインファクトリー
+发行: Design Factory、アイディアファクトリー株式会社、デザインファクトリー
+人物设定: RiRi
+主题歌作曲: 後藤康二
+主题歌演出: KENN
+原画: RiRi
+导演: 高村旭
+剧本: ふくだ りょうこ、有野幸、皆川千尋
+主题歌作词: 小川マキ
+官方网站: otomate.jp/yunohana_spring/switch/
+
+ゆのはなSpRING！
+开发: オトメイト、Design Factory、デザインファクトリー
+发行: Design Factory、アイディアファクトリー株式会社、デザインファクトリー
+剧本: 皆川千尋、有野幸、春夏秋冬よもひろ、ふくだりょうこ、夏越ちか
+人物设定: RiRi
+主题歌作曲: 後藤康二
+主题歌演出: KENN
+原画: RiRi
+导演: 高村旭
+音乐: 菱田拓海
+主题歌作词: 小川マキ
+链接: ErogameScape VNDB
+官方网站: otomate.jp/yunohana_spring/
+
+ゆのはなSpRING！ ～Cherishing Time～
+中文名: 温泉之花～Cherishing Time～
+开发: オトメイト、デザインファクトリー株式会社
+发行: アイディアファクトリー株式会社、デザインファクトリー株式会社
+剧本: 夏越ちか、有野幸、御門蓮、佐々木麿
+人物设定: RiRi
+主题歌演出: 霜月はるか、結良まり、Duca
+原画: RiRi
+导演: 高村旭
+官方网站: otomate.jp/yunohana_spring/ct/
