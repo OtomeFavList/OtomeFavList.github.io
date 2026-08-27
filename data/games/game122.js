06249228670c748dc481faef5167bd7b9bbb0019
OtomeFavList <otomefavlist@163.com>
2026-08-27 17:06:58 +0800

    Create game122.js

---
+++ data/games/game122.js
@@ -0,0 +1,12 @@
+私立ベルばら学園 ～ベルサイユのばらRe*imagination～
+开发: オトメイト、アイディアファクトリー
+发行: アイディアファクトリー株式会社、オトメイト
+剧本: 遠野チハル（故事编辑）ウサギリス(浦井アンナ／吉田夏美／清水花歩／高橋香里)
+音乐: 杉江一
+主题歌作曲: 阿部隆大、RIRIKO
+主题歌作词: RIRIKO、Uyu
+主题歌演出: ACRYLICSTAB、RIRIKO
+导演: 寺嶋桃子
+编辑: 遠野チハル
+链接: ErogameScape VNDB
+官方网站: otomate.jp/berubara-gakuen/
