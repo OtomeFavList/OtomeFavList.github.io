4dc66721b8a854e14ceb3ba385835fca204d8bba
OtomeFavList <otomefavlist@163.com>
2026-08-27 20:06:55 +0800

    Create game133.js

---
+++ data/games/game133.js
@@ -0,0 +1,10 @@
+ひめひび 1学期 -Princess Days-
+开发: TAKUYO
+剧本: 柚木隼人 小野真太郎　藤元（藤文）　Nao@猫の手
+主题歌作曲: 森藤晶司
+原画: 仁神ユキタカ
+主题歌作词: CAO、アキレスKEN
+主题歌演出: flaque
+http://www.takuyo.co.jp/products/himehibi_switch/
+http://www.takuyo.co.jp/products/himehibi/himehibi_portable.html
+http://www.takuyo.co.jp/products/himehibi/himehibivita/
