bf61de7870c725cb5b00fedb61964b59d6196b16
OtomeFavList <otomefavlist@163.com>
2026-08-27 20:10:17 +0800

    Create game134.js

---
+++ data/games/game134.js
@@ -0,0 +1,13 @@
+ひめひび -New Princess Days!!- 続! 二学期
+发行: TAKUYO
+剧本: 柚木隼人、ゆずしお
+人物设定: 仁神ユキタカ
+主题歌作曲: 森藤晶司
+主题歌作词: CAO、アキレスKEN
+主题歌演出: flaque
+原画: 仁神ユキタカ
+http://www.takuyo.co.jp/products/himehibi2_switch/
+http://www.takuyo.co.jp/products/himehibi2/
+
+ひめひび 続！二学期-New Princess Days!!-
+官方网站: takuyo.co.jp/products/himehibi/himehibivita/index.html
