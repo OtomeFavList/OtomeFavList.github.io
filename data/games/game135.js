ab9f8ae3b5c70cd2533362a694a7229a983e00c2
OtomeFavList <otomefavlist@163.com>
2026-08-27 20:15:36 +0800

    Create game135.js

---
+++ data/games/game135.js
@@ -0,0 +1,14 @@
+死神と少女
+中文名: 死神与少女
+开发: TAKUYO
+发行: TAKUYO
+剧本: 藤元（藤文）
+音乐: 森藤晶司
+人物设定: すみ兵
+主题歌作曲: love solfege、松本慎一郎
+主题歌作词: Simona Stanzani Pini、紺野比奈子
+主题歌演出: 綾野えいり、love solfege
+原画: すみ兵
+程序: 五十嵐亮太
+链接: ErogameScape VNDB
+官方网站: takuyo.co.jp/products/shinigami/
