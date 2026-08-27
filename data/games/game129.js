bcdd3efa047b3e67d141efd10102d68efbce20f2
OtomeFavList <otomefavlist@163.com>
2026-08-27 18:22:21 +0800

    Create game129.js

---
+++ data/games/game129.js
@@ -0,0 +1,23 @@
+殺し屋とストロベリー Plus
+开发: ブロッコリー
+发行: ブロッコリー
+游戏类型: 裏社会×恋愛ADV
+剧本: isora、ナミサキシキ
+导演: ナミサキシキ
+官方网站: korosuto.com/plus/
+
+殺し屋とストロベリー
+  中文名: 杀手与草莓
+开发: ブロッコリー
+发行: ブロッコリー
+剧本: isora、ナミサキ シキ、望月柚枝
+人物设定: カズキヨネ
+主题歌作曲: bermei.inazawa
+主题歌作词: lasah
+主题歌演出: lasah
+原作: ブロッコリー
+别名: Killer and Strawberry
+殺スト
+导演: ナミサキシキ
+链接: ErogameScape VNDB
+官方网站: korosuto.com/
