1e38c13d0fd22c919e4306622fee08c3e086a15d
OtomeFavList <otomefavlist@163.com>
2026-08-27 20:05:21 +0800

    Create game132.js

---
+++ data/games/game132.js
@@ -0,0 +1,13 @@
+ひめひび Another Princess Days ～ White or Black ～
+开发: TAKUYO
+发行: TAKUYO
+剧本: 井上愁、関口琴子、白鳥ユアン
+主题歌作曲: FAVORITE STEPS、安斎孝秋
+主题歌作词: FAVORITE STEPS、コツキミヤ
+主题歌演出: 若林倫香、紫苑雪
+人物设定: ふづき
+原画: ふづき
+制作总指挥: 田口茂宏
+音乐: タナカノブマサ
+链接: ErogameScape VNDB
+官方网站: takuyo.co.jp/products/himehibi_wb/
