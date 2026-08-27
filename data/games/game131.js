03086c924899fab0996459a842b215d4f7da58ef
OtomeFavList <otomefavlist@163.com>
2026-08-27 20:00:33 +0800

    Create game131.js

---
+++ data/games/game131.js
@@ -0,0 +1,13 @@
+SWEET CLOWN ～午前三時のオカシな道化師～
+开发: TAKUYO
+发行: TAKUYO
+剧本: 井上愁（古桥、真井线）、白鳥ユアン（久濑、密原线）、関口琴子（日之世线）
+人物设定: ヒロセアヅミ
+主题歌作曲: 柊奈緒、うたまろ（Nintendo Switch版）、ざっとん（Nintendo Switch版）
+主题歌作词: 柊奈緒、うたまろ（Nintendo Switch版）、ざっとん（Nintendo Switch版）
+主题歌演出: 柊奈緒、ラムソアーズ（Nintendo Switch版）
+原画: ヒロセアヅミ
+导演: 井上愁
+制作人: 齊藤幸治
+链接: ErogameScape VNDB
+官方网站: takuyo.co.jp/products/sweetclown/index.html
