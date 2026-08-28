468782b896c0cbed9b3641d584e8f6eb85931867
OtomeFavList <otomefavlist@163.com>
2026-08-29 04:48:48 +0800

    Create game140.js

---
+++ data/games/game140.js
@@ -0,0 +1,13 @@
+金色のコルダ3
+中文名: 金色琴弦 3
+开发: コーエーテクモホールディングス、ルビーパーティー
+发行: コーエーテクモホールディングス、コーエーテクモゲームス
+剧本: 杉原希
+人物设定: 呉由姫
+官方网站: gamecity.ne.jp/corda3/
+
+gamecity.ne.jp/corda3/special/
+
+gamecity.ne.jp/corda3/anothersky/index.htm
+
+gamecity.ne.jp/corda4/
