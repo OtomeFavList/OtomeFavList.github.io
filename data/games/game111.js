911da215b937d61528c17cde3b305e8546ce12c3
OtomeFavList <otomefavlist@163.com>
2026-08-28 19:25:56 +0800

    Update game111.js

--- data/games/game111.js
+++ data/games/game111.js
@@ -1,17 +1,3 @@
-中文名: 穿军靴的猫
-开发: Primula
-发行: Primula、株式会社プロトタイプ 、PRODUCTION PENCIL
-剧本: 藤文、千草、清水月子
-音乐: solfa
-人物设定: 紫真依、稲葉せいこ（SD）
-主题歌演出: Ether
-原画: 紫真依
-导演: 犬小屋権太
-SD原画: 稲葉せいこ
-企画: Primula
-别名: 軍猫
-官方网站: primula.jpn.com/gunneko/
-
 // ==========【单个游戏独立数据模板｜新版ESModule】==========
 // 新增游戏操作：复制本文件，修改所有信息、唯一ID、图片路径
 // 图片路径采用相对路径格式（game/xxx.jpg 或 char/xxx/xxx.jpg）
