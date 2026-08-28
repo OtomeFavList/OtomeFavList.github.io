58918defbd1d4efcb32900c8177a1dc847c37d9b
OtomeFavList <otomefavlist@163.com>
2026-08-29 04:30:47 +0800

    Create game138.js

---
+++ data/games/game138.js
@@ -0,0 +1,41 @@
+神凪ノ杜 五月雨綴り
+开发: Matatabi
+发行: Matatabi、ワークマン
+主题歌作曲: 髙木隆次、高木隆次
+主题歌作词: 月宮うさぎ
+主题歌演出: Annabel
+平台:
+PSV Nintendo Switch
+剧本: 朝比奈夏樹
+人物设定: 豌豆まめ丸
+原画: 豌豆まめ丸
+背景: meiz
+企画: 恵曇あや
+音乐: 有限会社ジェオ
+官方网站: matatabi.tv/kannagi/
+
+神凪ノ杜 妖狐奇譚
+开发: Matatabi
+剧本: 朝比奈夏樹
+人物设定: 豌豆まめ丸
+主题歌作曲: 髙木隆次、高木隆次
+主题歌作词: 月宮うさぎ
+主题歌演出: Annabel
+原画: 豌豆まめ丸
+导演: 恵曇あや
+音乐: 有限会社ジェオ
+链接: ErogameScape VNDB Getchu
+官方网站: matatabi.tv/kannagi/
+
+神凪ノ杜 龍神奇譚
+开发: Matatabi
+剧本: 朝比奈夏樹
+音乐: 須藤祐
+人物设定: 豌豆まめ丸
+主题歌作曲: 髙木隆次、高木隆次
+主题歌作词: 月宮うさぎ
+主题歌演出: Annabel
+原画: 豌豆まめ丸
+企画: 恵曇あや
+链接: ErogameScape VNDB Getchu
+官方网站: matatabi.tv/kannagi/
