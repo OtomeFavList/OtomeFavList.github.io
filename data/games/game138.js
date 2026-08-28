282126d27b44ed1e0ba90b60635e619ea7bd9914
OtomeFavList <otomefavlist@163.com>
2026-08-29 04:04:59 +0800

    Create game138.js

---
+++ data/games/game138.js
@@ -0,0 +1,11 @@
+Sullyland Nursery Rhyme
+开发: オトメイト、デザインファクトリー
+发行: アイディアファクトリー
+剧本: 夏野景
+主题歌演出: cadode、亜咲花
+原画: miko
+导演: 黒輪脩、一ジョー
+别名: サリライ
+サリーランドナーサリーライム
+平台: Nintendo Switch
+官方网站: otomate.jp/sullyland/
