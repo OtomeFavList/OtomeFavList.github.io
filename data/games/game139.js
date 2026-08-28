f8774332867bac4c7b17613d4dbb812d7ff61742
OtomeFavList <otomefavlist@163.com>
2026-08-29 04:44:52 +0800

    Create game139.js

---
+++ data/games/game139.js
@@ -0,0 +1,17 @@
+金色のコルダ
+开发: コーエー、ルビーパーティー
+发行: コーエー
+剧本: 加藤正人
+音乐: YUKI NAKANO
+人物设定: 呉由姫
+别名: 金色琴弦
+官方网站: gamecity.ne.jp/corda/countdown_pc/index.htm
+
+
+gamecity.ne.jp/ps2/corda2/index02.htm
+gamecity.ne.jp/psp/corda2/
+
+gamecity.ne.jp/ps2/corda2/encore/
+gamecity.ne.jp/psp/corda2/encore/
+
+gamecity.ne.jp/corda2ff/
