2e8bcf805043972982bc51fe03a040a2a29a1c4d
OtomeFavList <otomefavlist@163.com>
2026-08-30 04:09:00 +0800

    Update game121.js

--- data/games/game121.js
+++ data/games/game121.js
@@ -7,7 +7,7 @@
 // 官中发售以后，需要修改：游戏名、发售年份、汉化厂商、封面图、角色名
 const gameData = {
     id: "game121", // 全局唯一ID，不可重复，如 game001
-    name: "忍び、恋うつつ-",
+    name: "忍び、恋うつつ",
     year: "2020",
     publisher: ["Otomate"],
     cnStudio: "暂无",
