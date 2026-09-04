56efe3d3bda3463a3deae215a9df87e46b59680a
OtomeFavList <otomefavlist@163.com>
2026-09-05 02:30:14 +0800

    Update game010.js

--- data/games/game010.js
+++ data/games/game010.js
@@ -100,292 +100,301 @@ const gameData = {
                             "char/010/Ukai2.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 久世鹟
         {
             id: "g010_s01",
-            name: "配角",
+            name: "久世鹟",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Hitaki.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 稚子谷新
         {
             id: "g010_s02",
-            name: "配角",
+            name: "稚子谷新",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Kijitani.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 柾小瑠璃
         {
             id: "g010_s03",
-            name: "配角",
+            name: "柾小瑠璃",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Koruri.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 杙梛
         {
             id: "g010_s04",
-            name: "配角",
+            name: "杙梛",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Kuina.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 猿子基史
         {
             id: "g010_s05",
-            name: "配角",
+            name: "猿子基史",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Mashiko.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 百舌山识郎
         {
             id: "g010_s06",
-            name: "配角",
+            name: "百舌山识郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Mozuyama.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 莺地启三郎
         {
             id: "g010_s07",
-            name: "配角",
+            name: "莺地启三郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Ouchi.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 尾鷲英树
         {
             id: "g010_s08",
-            name: "配角",
+            name: "尾鷲英树",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Owase.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 笹乞藤一郎
         {
             id: "g010_s09",
-            name: "配角",
+            name: "笹乞藤一郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Sasagoi.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 四木沼蔷子
         {
             id: "g010_s10",
-            name: "配角",
+            name: "四木沼蔷子",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Shoko.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 四木沼乔
         {
             id: "g010_s11",
-            name: "配角",
+            name: "四木沼乔",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Takashi.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 燕野太郎
         {
             id: "g010_s12",
-            name: "配角",
+            name: "燕野太郎",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Taro.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 朱鹭宫刊
         {
             id: "g010_s13",
-            name: "配角",
+            name: "朱鹭宫刊",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Tokimiya.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 苇切拓真
         {
             id: "g010_s14",
-            name: "配角",
+            name: "苇切拓真",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Yoshikiri.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 隐由鹰
         {
             id: "g010_s15",
-            name: "配角",
+            name: "隐由鹰",
             gender: "male",
             isHidden: false,
             isFD: false,
             isSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Yutaka.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 小原秋沙
         {
-            id: "g010_s16",
-            name: "配角",
+            id: "g010_fs01",
+            name: "小原秋沙",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Aisa.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 風見千鳥
         {
-            id: "g010_s17",
-            name: "配角",
+            id: "g010_fs02",
+            name: "風見千鳥",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Chitori.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // ハンス·ハメル
         {
-            id: "g010_s18",
-            name: "配角",
+            id: "g010_fs03",
+            name: "ハンス·ハメル",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Hans.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 錐合蓮
         {
-            id: "g010_s19",
-            name: "配角",
+            id: "g010_fs04",
+            name: "錐合蓮",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Kiriai.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 鵠居栄
         {
-            id: "g010_s20",
-            name: "配角",
+            id: "g010_fs05",
+            name: "鵠居栄",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Kugei.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 雀部忠清
         {
-            id: "g010_s21",
-            name: "配角",
+            id: "g010_fs06",
+            name: "雀部忠清",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Sasabe.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 黎雪加
         {
-            id: "g010_s22",
-            name: "配角",
+            id: "g010_fs07",
+            name: "黎雪加",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Sekka.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 鵜飼昌造
         {
-            id: "g010_s23",
-            name: "配角",
+            id: "g010_fs08",
+            name: "鵜飼昌造",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Shouzou.jpg"], type: "base" }
             ]
         },
-        // 次要角色（isSub=true → 开关开启才显示整个角色卡片）
+        // 鵺野桐彦
         {
-            id: "g010_s24",
-            name: "配角",
+            id: "g010_fs09",
+            name: "鵺野桐彦",
             gender: "male",
             isHidden: false,
             isFD: false,
-            isSub: true,
+            isSub: false,
+            isFdSub: true,
             images: [
-                { srcList: ["char/g001_fd01_1.jpg"], type: "base" }
+                { srcList: ["char/010/Yano.jpg"], type: "base" }
             ]
         }
     ]
