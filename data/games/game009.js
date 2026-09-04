7f2ece599668ec65287e87ca3bd74841f9c7df94
OtomeFavList <otomefavlist@163.com>
2026-09-04 16:30:12 +0800

    Update game009.js

--- data/games/game009.js
+++ data/games/game009.js
@@ -98,23 +98,14 @@ const gameData = {
             images: [
                 { srcList: ["char/009/Peter.jpg",
                             "char/009/Peter2.jpg",
-                            "char/009/Peter3.jpg"], type: "base" }
-            ]
-        },
-        // 伊萊·奧马尔
-        {
-            id: "g009_fd01",
-            name: "伊萊·奧马尔",
-            gender: "male",
-            isHidden: false,
-            isFD: true,
-            images: [
-                { srcList: ["char/009/Eli.jpg"], type: "base" }
+                            "char/009/Peter3.jpg",
+                            "char/009/Peter4.jpg",
+                            "char/009/Peter5.jpg"], type: "base" }
             ]
         },
         // 梅列尼斯·莱文
         {
-            id: "g009_fd02",
+            id: "g009_fd01",
             name: "梅列尼斯·莱文",
             gender: "male",
             isHidden: false,
@@ -125,14 +116,127 @@ const gameData = {
         },
         // 欧文·赫里欧
         {
-            id: "g009_fd03",
+            id: "g009_fd02",
             name: "欧文·赫里欧",
             gender: "male",
             isHidden: false,
             isFD: true,
+            isSub: true,
             images: [
                 { srcList: ["char/009/Owen.jpg",
                             "char/009/Owen2.jpg"], type: "base" }
+            ]
+        },
+        // 凯萨琳·斯贝德
+        {
+            id: "g009_s01",
+            name: "凯萨琳·斯贝德",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Catherine.jpg",
+                            "char/009/Catherine2.jpg"], type: "base" }
+            ]
+        },
+        // 克拉莉丝·缇亚
+        {
+            id: "g009_s02",
+            name: "克拉莉丝·缇亚",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Claris.jpg",
+                            "char/009/Claris2.jpg"], type: "base" }
+            ]
+        },
+        // 伊萊·奧马尔
+        {
+            id: "g009_s03",
+            name: "伊萊·奧马尔",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Eli.jpg"], type: "base" }
+            ]
+        },
+        // 玛斯
+        {
+            id: "g009_s04",
+            name: "玛斯",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Mars.jpg",
+                            "char/009/Mars2.jpg"], type: "base" }
+            ]
+        },
+        // 米奈娃
+        {
+            id: "g009_s05",
+            name: "米奈娃",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Minerva.jpg",
+                            "char/009/Minerva2.jpg"], type: "base" }
+            ]
+        },
+        // 雷·安德
+        {
+            id: "g009_s06",
+            name: "雷·安德",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Ray.jpg"], type: "base" }
+            ]
+        },
+        // 罗宾·布雷特
+        {
+            id: "g009_s07",
+            name: "配角",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Robin.jpg"], type: "base" }
+            ]
+        },
+        // 西尔维·史考特
+        {
+            id: "g009_s08",
+            name: "西尔维·史考特",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Sylvie.jpg"], type: "base" }
+            ]
+        },
+        // 宙斯
+        {
+            id: "g009_s09",
+            name: "宙斯",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            images: [
+                { srcList: ["char/009/Zeus.jpg"], type: "base" }
             ]
         }
     ]
