ecf7e4f9bce797644ca02aeedcbbb0d023a2a3c2
OtomeFavList <otomefavlist@163.com>
2026-09-12 17:46:03 +0800

    Update game018.js

--- data/games/game018.js
+++ data/games/game018.js
@@ -26,7 +26,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Koharu.jpg"], type: "base" }
+                { srcList: ["char/018/Koharu.jpg",
+                            "char/018/Koharu2.jpg",
+                            "char/018/Koharu3.jpg",
+                            "char/018/Koharu4.jpg"], type: "base" }
             ]
         },
         // 久我深琴
@@ -37,7 +40,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Mikoto.jpg"], type: "base" }
+                { srcList: ["char/018/Mikoto.jpg",
+                            "char/018/Mikoto2.jpg",
+                            "char/018/Mikoto3.jpg",
+                            "char/018/Mikoto4.jpg"], type: "base" }
             ]
         },
         // 不知火七海
@@ -48,7 +54,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Nanami.jpg"], type: "base" }
+                { srcList: ["char/018/Nanami.jpg",
+                            "char/018/Nanami2.jpg",
+                            "char/018/Nanami3.jpg",
+                            "char/018/Nanami4.jpg"], type: "base" }
             ]
         },
         // 宿史晓人
@@ -59,7 +68,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Akito.jpg"], type: "base" }
+                { srcList: ["char/018/Akito.jpg",
+                            "char/018/Akito2.jpg",
+                            "char/018/Akito3.jpg",
+                            "char/018/Akito4.jpg"], type: "base" }
             ]
         },
         // 乙丸平士
@@ -70,7 +82,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Heishi.jpg"], type: "base" }
+                { srcList: ["char/018/Heishi.jpg",
+                            "char/018/Heishi2.jpg",
+                            "char/018/Heishi3.jpg",
+                            "char/018/Heishi4.jpg"], type: "base" }
             ]
         },
         // 加贺见一月
@@ -81,7 +96,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Itsuki.jpg"], type: "base" }
+                { srcList: ["char/018/Itsuki.jpg",
+                            "char/018/Itsuki2.jpg",
+                            "char/018/Itsuki3.jpg",
+                            "char/018/Itsuki4.jpg"], type: "base" }
             ]
         },
         // 结贺驱
@@ -92,7 +110,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Kakeru.jpg"], type: "base" }
+                { srcList: ["char/018/Kakeru.jpg",
+                            "char/018/Kakeru2.jpg",
+                            "char/018/Kakeru3.jpg",
+                            "char/018/Kakeru4.jpg"], type: "base" }
             ]
         },
         // 远矢正宗
@@ -103,7 +124,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Masamune.jpg"], type: "base" }
+                { srcList: ["char/018/Masamune.jpg",
+                            "char/018/Masamune2.jpg",
+                            "char/018/Masamune3.jpg",
+                            "char/018/Masamune4.jpg"], type: "base" }
             ]
         },
         // 吾妻夏彦
@@ -114,7 +138,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Natsuhiko.jpg"], type: "base" }
+                { srcList: ["char/018/Natsuhiko.jpg",
+                            "char/018/Natsuhiko2.jpg",
+                            "char/018/Natsuhiko3.jpg",
+                            "char/018/Natsuhiko4.jpg"], type: "base" }
             ]
         },
         // 二条朔也
@@ -125,7 +152,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Sakuya.jpg"], type: "base" }
+                { srcList: ["char/018/Sakuya.jpg",
+                            "char/018/Sakuya2.jpg",
+                            "char/018/Sakuya3.jpg",
+                            "char/018/Sakuya4.jpg"], type: "base" }
             ]
         },
         // 市之濑千里
@@ -136,7 +166,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Senri.jpg"], type: "base" }
+                { srcList: ["char/018/Senri.jpg",
+                            "char/018/Senri2.jpg",
+                            "char/018/Senri3.jpg",
+                            "char/018/Senri4.jpg"], type: "base" }
             ]
         },
         // 室星朗
@@ -147,7 +180,114 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/018/Ron.jpg"], type: "base" }
+                { srcList: ["char/018/Ron.jpg",
+                            "char/018/Ron2.jpg",
+                            "char/018/Ron3.jpg",
+                            "char/018/Ron4.jpg"], type: "base" }
+            ]
+        },
+        // 艾因
+        {
+            id: "g018_s01",
+            name: "爱音",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/018/Aion.jpg"], type: "base" }
+            ]
+        },
+        // 泷岛雪
+        {
+            id: "g018_s02",
+            name: "泷岛雪",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/018/Setsu.jpg"], type: "base" }
+            ]
+        },
+        // 结贺史狼
+        {
+            id: "g018_s03",
+            name: "结贺史狼",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/018/Shirou.jpg"], type: "base" }
+            ]
+        },
+        // 铃原空汰
+        {
+            id: "g018_s04",
+            name: "铃原空汰",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/018/Sorata.jpg"], type: "base" }
+            ]
+        },
+        // 加贺见和叶
+        {
+            id: "g018_fs01",
+            name: "加贺见和叶",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/018/Kazuha.jpg"], type: "base" }
+            ]
+        },
+        // 远矢基久
+        {
+            id: "g018_fs02",
+            name: "远矢基久",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/018/Motohisa.jpg"], type: "base" }
+            ]
+        },
+        // 徒徒枝凪沙
+        {
+            id: "g018_fs03",
+            name: "徒徒枝凪沙",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/018/Nagisa.jpg"], type: "base" }
+            ]
+        },
+        // 西所弥广
+        {
+            id: "g018_fs04",
+            name: "西所弥广",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/018/Yahiro.jpg"], type: "base" }
             ]
         }
     ]
