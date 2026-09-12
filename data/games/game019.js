730e0dbdf98ea200660cd0c02ebde03a5cf43205
OtomeFavList <otomefavlist@163.com>
2026-09-12 21:22:08 +0800

    Update game019.js

--- data/games/game019.js
+++ data/games/game019.js
@@ -25,7 +25,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Chizuru.jpg"], type: "base" }
+                { srcList: ["char/019/Chizuru.jpg",
+                            "char/019/Chizuru2.jpg",
+                            "char/019/Chizuru3.jpg"], type: "base" },
+                { srcList: ["char/019/Chizuru4.jpg"], type: "fd" }
             ]
         },
         // 斋藤一
@@ -36,7 +39,12 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Hajime.jpg"], type: "base" }
+                { srcList: ["char/019/Hajime.jpg",
+                            "char/019/Hajime2.jpg",
+                            "char/019/Hajime3.jpg",
+                            "char/019/Hajime4.jpg",
+                            "char/019/Hajime5.jpg"], type: "base" },
+                { srcList: ["char/019/Hajime6.jpg"], type: "fd" }
             ]
         },
         // 原田左之助
@@ -47,7 +55,12 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Harada.jpg"], type: "base" }
+                { srcList: ["char/019/Harada.jpg",
+                            "char/019/Harada2.jpg",
+                            "char/019/Harada3.jpg",
+                            "char/019/Harada4.jpg",
+                            "char/019/Harada5.jpg"], type: "base" },
+                { srcList: ["char/019/Harada6.jpg"], type: "fd" }
             ]
         },
         // 藤堂平助
@@ -58,7 +71,12 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Heisuke.jpg"], type: "base" }
+                { srcList: ["char/019/Heisuke.jpg",
+                            "char/019/Heisuke2.jpg",
+                            "char/019/Heisuke3.jpg",
+                            "char/019/Heisuke4.jpg",
+                            "char/019/Heisuke5.jpg"], type: "base" },
+                { srcList: ["char/019/Harada6.jpg"], type: "fd" }
             ]
         },
         // 伊庭八郎
@@ -69,7 +87,9 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Iba.jpg"], type: "base" }
+                { srcList: ["char/019/Iba.jpg",
+                            "char/019/Iba2.jpg",
+                            "char/019/Iba3.jpg"], type: "base" }
             ]
         },
         // 风间千景
@@ -80,7 +100,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Kazama.jpg"], type: "base" }
+                { srcList: ["char/019/Kazama.jpg",
+                            "char/019/Kazama2.jpg",
+                            "char/019/Kazama3.jpg"], type: "base" },
+                { srcList: ["char/019/Kazama4.jpg"], type: "fd" }
             ]
         },
         // 山南敬助
@@ -91,7 +114,11 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Keisuke.jpg"], type: "base" }
+                { srcList: ["char/019/Keisuke.jpg",
+                            "char/019/Keisuke2.jpg",
+                            "char/019/Keisuke3.jpg",
+                            "char/019/Keisuke4.jpg"], type: "base" },
+                { srcList: ["char/019/Keisuke5.jpg"], type: "fd" }
             ]
         },
         // 坂本龙马
@@ -102,7 +129,10 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Ryouma.jpg"], type: "base" }
+                { srcList: ["char/019/Ryouma.jpg",
+                            "char/019/Ryouma2.jpg",
+                            "char/019/Ryouma3.jpg",
+                            "char/019/Ryouma4.jpg"], type: "base" }
             ]
         },
         // 永仓新八
@@ -113,7 +143,12 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Shinpachi.jpg"], type: "base" }
+                { srcList: ["char/019/Shinpachi.jpg",
+                            "char/019/Shinpachi2.jpg",
+                            "char/019/Shinpachi3.jpg",
+                            "char/019/Shinpachi4.jpg",
+                            "char/019/Shinpachi5.jpg"], type: "base" },
+                { srcList: ["char/019/Shinpachi6.jpg"], type: "fd" }
             ]
         },
         // 冲田总司
@@ -124,7 +159,11 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Souji.jpg"], type: "base" }
+                { srcList: ["char/019/Souji.jpg",
+                            "char/019/Souji2.jpg",
+                            "char/019/Souji3.jpg",
+                            "char/019/Souji4.jpg"], type: "base" },
+                { srcList: ["char/019/Souji5.jpg"], type: "fd" }
             ]
         },
         // 相马主计
@@ -135,7 +174,11 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Souma.jpg"], type: "base" }
+                { srcList: ["char/019/Souma.jpg",
+                            "char/019/Souma2.jpg",
+                            "char/019/Souma3.jpg",
+                            "char/019/Souma4.jpg",
+                            "char/019/Souma5.jpg"], type: "base" }
             ]
         },
         // 土方岁三
@@ -146,7 +189,11 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Toshizou.jpg"], type: "base" }
+                { srcList: ["char/019/Toshizou.jpg"],
+                            "char/019/Toshizou2.jpg",
+                            "char/019/Toshizou3.jpg",
+                            "char/019/Toshizou4.jpg", type: "base" },
+                { srcList: ["char/019/Toshizou5.jpg"], type: "fd" }
             ]
         },
         // 山崎烝
@@ -157,7 +204,392 @@ const gameData = {
             isHidden: false,
             isFD: false,
             images: [
-                { srcList: ["char/019/Yamazaki.jpg"], type: "base" }
+                { srcList: ["char/019/Yamazaki.jpg",
+                            "char/019/Yamazaki2.jpg",
+                            "char/019/Yamazaki3.jpg"], type: "base" },
+                { srcList: ["char/019/Yamazaki4.jpg"], type: "fd" }
+            ]
+        },
+        // 井上源三郎
+        {
+            id: "g019_s01",
+            name: "井上源三郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Genzaburou.jpg",
+                            "char/019/Genzaburou2.jpg"], type: "base" },
+                { srcList: ["char/019/Genzaburou3.jpg"], type: "fd" }
+            ]
+        },
+        // 近藤勇
+        {
+            id: "g019_s02",
+            name: "近藤勇",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Isami.jpg"], type: "base" },
+                { srcList: ["char/019/Isami2.jpg"], type: "fd" }
+            ]
+        },
+        // 岛田魁
+        {
+            id: "g019_s03",
+            name: "岛田魁",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kai.jpg"], type: "base" },
+                { srcList: ["char/019/Kai2.jpg"], type: "fd" }
+            ]
+        },
+        // 武田观柳斋
+        {
+            id: "g019_s04",
+            name: "武田观柳斋",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kanryuusai.jpg",
+                            "char/019/Kanryuusai2.jpg"], type: "base" }
+            ]
+        },
+        // 南云熏
+        {
+            id: "g019_s05",
+            name: "南云熏",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kaoru.jpg"], type: "base" },
+                { srcList: ["char/019/Kaoru2.jpg"], type: "fd" }
+            ]
+        },
+        // 伊东甲子太郎
+        {
+            id: "g019_s06",
+            name: "伊东甲子太郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kashitarou.jpg",
+                            "char/019/Kashitarou2.jpg"], type: "base" }
+            ]
+        },
+        // 君菊
+        {
+            id: "g019_s07",
+            name: "配角",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kimijiku.jpg",
+                            "char/019/Kimijiku2.jpg"], type: "base" }
+            ]
+        },
+        // 小铃
+        {
+            id: "g019_s08",
+            name: "小铃",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kosuzu.jpg",
+                            "char/019/Kosuzu2.jpg"], type: "base" },
+                { srcList: ["char/019/Kosuzu3.jpg"], type: "fd" }
+            ]
+        },
+        // 本田小太郎
+        {
+            id: "g019_s09",
+            name: "本田小太郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kotarou.jpg"], type: "base" }
+            ]
+        },
+        // 雪村纲道
+        {
+            id: "g019_s10",
+            name: "雪村纲道",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Koudou.jpg"], type: "base" }
+            ]
+        },
+        // 不知火匡
+        {
+            id: "g019_s11",
+            name: "不知火匡",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kyou.jpg"], type: "base" },
+                { srcList: ["char/019/Kyou2.jpg"], type: "fd" }
+            ]
+        },
+        // 天雾九寿
+        {
+            id: "g019_s12",
+            name: "天雾九寿",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Kyujyu.jpg"], type: "base" },
+                { srcList: ["char/019/Kyujyu2.jpg"], type: "fd" }
+            ]
+        },
+        // 大岛圭介
+        {
+            id: "g019_s13",
+            name: "大岛圭介",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Otori.jpg"], type: "base" }
+            ]
+        },
+        // 野村利三郎
+        {
+            id: "g019_s14",
+            name: "野村利三郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Risaburou.jpg"], type: "base" }
+            ]
+        },
+        // 松本良顺
+        {
+            id: "g019_s15",
+            name: "松本良顺",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Ryoujyun.jpg"], type: "base" }
+            ]
+        },
+        // 井吹龙之介
+        {
+            id: "g019_s16",
+            name: "井吹龙之介",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Ryuunosuke.jpg",
+                            "char/019/Ryuunosuke2.jpg"], type: "base" },
+                { srcList: ["char/019/Ryuunosuke3.jpg"], type: "fd" }
+            ]
+        },
+        // 三木三郎
+        {
+            id: "g019_s17",
+            name: "三木三郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Saburou.jpg",
+                            "char/019/Saburou2.jpg"], type: "base" }
+            ]
+        },
+        // 千姬
+        {
+            id: "g019_s18",
+            name: "千姬",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Senhime.jpg",
+                            "char/019/Senhime2.jpg"], type: "base" },
+                { srcList: ["char/019/Senhime3.jpg"], type: "fd" }
+            ]
+        },
+        // 中冈慎太郎
+        {
+            id: "g019_s19",
+            name: "中冈慎太郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Shintarou.jpg"], type: "base" }
+            ]
+        },
+        // 榎本武扬
+        {
+            id: "g019_s20",
+            name: "榎本武扬",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: true,
+            isFdSub: false,
+            images: [
+                { srcList: ["char/019/Takeaki.jpg"], type: "base" }
+            ]
+        },
+        // 佐佐木爱次郎
+        {
+            id: "g019_fs01",
+            name: "佐佐木爱次郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Aijirou.jpg"], type: "base" }
+            ]
+        },
+        // 平间重助
+        {
+            id: "g019_fs02",
+            name: "平间重助",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Jyuusuke.jpg"], type: "base" }
+            ]
+        },
+        // 芹沢鸭
+        {
+            id: "g019_fs03",
+            name: "芹沢鸭",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Kamo.jpg"], type: "base" },
+                { srcList: ["char/019/Kamo2.jpg"], type: "fd" }
+            ]
+        },
+        // 松原忠司
+        {
+            id: "g019_fs04",
+            name: "松原忠司",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Matsubara.jpg"], type: "base" }
+            ]
+        },
+        // 新见锦
+        {
+            id: "g019_fs05",
+            name: "新见锦",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Nishiki.jpg"], type: "base" }
+            ]
+        },
+        // 谷三十郎
+        {
+            id: "g019_fs06",
+            name: "谷三十郎",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Tani.jpg"], type: "base" }
+            ]
+        },
+        // 梅
+        {
+            id: "g019_fs07",
+            name: "梅",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Ume.jpg"], type: "base" }
+            ]
+        },
+        // 殿内义雄
+        {
+            id: "g019_fs08",
+            name: "殿内义雄",
+            gender: "male",
+            isHidden: false,
+            isFD: false,
+            isSub: false,
+            isFdSub: true,
+            images: [
+                { srcList: ["char/019/Yoshio.jpg"], type: "base" }
             ]
         }
     ]
